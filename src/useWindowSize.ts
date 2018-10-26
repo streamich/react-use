2c46899b493164e3c60f412f3914a6cbbc45db49
streamich <streamich@gmail.com>
2018-10-27 01:18:55 +0200

    feat: 🎸 add useWindowSize() hook

---
+++ src/useWindowSize.ts
@@ -0,0 +1,25 @@
+import {useState, useEffect} from './react';
+
+const isClient = typeof window === 'object';
+
+const useWindowSize = (initialWidth = Infinity, initialHeight = Infinity) => {
+  const [state, setState] = useState<{width: number, height: number}>({
+    width: isClient ? window.innerWidth : initialWidth,
+    height: isClient ? window.innerHeight : initialHeight,
+  });
+  
+  useEffect(() => {
+    const handler = () => {
+      setState({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+    window.addEventListener('resize', handler);
+    return () => window.removeEventListener('resize', handler);
+  }, [1]);
+
+  return state;
+};
+
+export default useWindowSize;
