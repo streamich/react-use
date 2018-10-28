33ed27fc12b8d00fcdb229d85cb7edd8bdc1a94f
streamich <streamich@gmail.com>
2018-10-28 21:25:41 +0000

    Updates

--- lib/useToggle.d.ts
+++ lib/useToggle.d.ts
@@ -1,5 +1,4 @@
 export declare type UseToggle = (state: boolean) => [boolean, // state
-() => void, // toggle
-(state: boolean) => void];
+(nextValue?: boolean) => void];
 declare const useToggle: UseToggle;
 export default useToggle;
