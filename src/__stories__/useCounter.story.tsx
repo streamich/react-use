395e82b03bb41c4e7bc849bfc4cfb50611fbae3b
streamich <streamich@gmail.com>
2018-10-29 18:42:47 +0100

    feat: 🎸 improve useCounter interface
    BREAKING CHANGE: useCounter interface changed

--- src/__stories__/useCounter.story.tsx
+++ src/__stories__/useCounter.story.tsx
@@ -4,13 +4,18 @@ import {useCounter} from '..';
 import ShowDocs from '../util/ShowDocs';
 
 const Demo = () => {
-  const [value, inc, set] = useCounter();
+  const [value, {inc, dec, get, set, reset}] = useCounter(5);
 
   return (
     <div>
-      <div>{value}</div>
+      <div>{value} is {get()}</div>
       <button onClick={() => inc()}>Increment</button>
+      <button onClick={() => dec()}>Decrement</button>
+      <button onClick={() => inc(5)}>Increment (+5)</button>
+      <button onClick={() => dec(5)}>Decrement (-5)</button>
       <button onClick={() => set(100)}>Set 100</button>
+      <button onClick={() => reset()}>Reset</button>
+      <button onClick={() => reset(25)}>Reset (25)</button>
     </div>
   );
 };
--- src/useCounter.ts
+++ src/useCounter.ts
@@ -1,12 +1,31 @@
-import {useState} from './react';
+import useGetSet from './useGetSet';
+import {useCallback} from './react';
 
-export type Increment = (inc?: number) => void;
-export type Set = (value: number) => void;
+export interface CounterActions {
+  inc: (delta?: number) => void;
+  dec: (delta?: number) => void;
+  get: () => number;
+  set: (value: number) => void;
+  reset: (value?: number) => void;
+}
 
-const useCounter = (initialValue: number = 0): [number, Increment, Set] => {
-  const [value, set] = useState<number>(initialValue);
+const useCounter = (initialValue: number = 0): [number, CounterActions] => {
+  const [get, set] = useGetSet<number>(initialValue);
+  const inc = useCallback((delta: number = 1) => set(get() + delta), []);
+  const dec = useCallback((delta: number = 1) => inc(-delta), []);
+  const reset = useCallback((value: number = initialValue) => {
+    initialValue = value;
+    set(value);
+  }, []);
+  const actions = {
+    inc,
+    dec,
+    get,
+    set,
+    reset,
+  };
 
-  return [value, (inc = 1) => set(value + inc), set];
+  return [get(), actions];
 };
 
 export default useCounter;
