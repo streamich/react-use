25f63148ccc7a63640a7a87ccacad37d04075751
streamich <streamich@gmail.com>
2018-10-30 17:03:54 +0000

    Updates

--- lib/useLocalStorage.d.ts
+++ lib/useLocalStorage.d.ts
@@ -1,2 +1,2 @@
-declare const useLocalStorage: (key: string) => string | undefined;
+declare const useLocalStorage: <T>(key: string, initialValue?: T | undefined, raw?: boolean | undefined) => [T, (value: T) => void];
 export default useLocalStorage;
