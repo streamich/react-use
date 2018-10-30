c2aee592af6beb8ba12d6a93207fbb5c9c362512
streamich <streamich@gmail.com>
2018-10-30 18:18:51 +0100

    feat: 🎸 add useHoverDirty hook

---
+++ src/useHoverDirty.ts
@@ -0,0 +1,33 @@
+import {useEffect, useState} from './react';
+
+// kudos: https://usehooks.com/
+const useHoverDirty = (ref) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if ((typeof ref !== 'object') || (typeof ref.current === 'undefined')) {
+      throw new TypeError('useHoverDirty expects a single ref argument.');
+    }
+  }
+
+  const [value, setValue] = useState(false);
+
+  useEffect(() => {
+    const onMouseOver = () => setValue(true);
+    const onMouseOut = () => setValue(false);
+
+    if (ref && ref.current) {
+      ref.current.addEventListener('mouseover', onMouseOver);
+      ref.current.addEventListener('mouseout', onMouseOut);
+    }
+
+    return () => {
+      if (ref && ref.current) {
+        ref.current.removeEventListener('mouseover', onMouseOver);
+        ref.current.removeEventListener('mouseout', onMouseOut);
+      }
+    };
+  }, []);
+
+  return value;
+}
+
+export default useHoverDirty;
