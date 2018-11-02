bfd114a4072e4770cfc8ab551cc6ce9d8f237b2f
Li Yan <lidoyan@gmail.com>
2018-11-02 15:50:04 +0800

    feat: allow useSetState setter to accept function
    * make setState of useSetState accept function argument
    
    * update storybook

--- src/useSetState.ts
+++ src/useSetState.ts
@@ -1,10 +1,16 @@
 import {useState} from './react';
 
-const useSetState = <T extends object>(initialState: T = {} as T): [T, (patch: Partial<T>) => void]=> {
+const useSetState = <T extends object>(initialState: T = {} as T): [T, (patch: Partial<T> | Function) => void]=> {
   const [state, set] = useState<T>(initialState);
   const setState = (patch) => {
-    Object.assign(state, patch);
-    set(state);
+    if (patch instanceof Function) {
+      set((prevState) => {
+          return Object.assign(state, patch(prevState))
+      })
+    } else {
+      Object.assign(state, patch);
+      set(state);
+    }
   };
 
   return [state, setState];
