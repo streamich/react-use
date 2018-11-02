c4bca9300d83964cc6ce8df8b79e61562b0fdd82
streamich <streamich@gmail.com>
2018-11-02 07:51:09 +0000

    Updates

--- lib/react.d.ts
+++ lib/react.d.ts
@@ -1,4 +1,4 @@
-export declare type UseState = <T>(initialState: T | (() => T)) => [T, (newState: T) => void];
+export declare type UseState = <T>(initialState: T | (() => T)) => [T, (newState: T | ((newState: any) => T)) => void];
 export declare const useState: UseState;
 export declare type UseEffect = (didUpdate: () => ((() => void) | void), params?: any[]) => void;
 export declare const useEffect: UseEffect;
--- lib/useSetState.d.ts
+++ lib/useSetState.d.ts
@@ -1,2 +1,2 @@
-declare const useSetState: <T extends object>(initialState?: T) => [T, (patch: Partial<T>) => void];
+declare const useSetState: <T extends object>(initialState?: T) => [T, (patch: Function | Partial<T>) => void];
 export default useSetState;
