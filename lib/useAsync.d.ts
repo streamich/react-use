ba9f610bdaa854ce8038c7a83a986b711385e5bc
streamich <streamich@gmail.com>
2018-12-17 17:47:42 +0000

    Updates

--- lib/useAsync.d.ts
+++ lib/useAsync.d.ts
@@ -1,7 +1,15 @@
-export interface AsyncState<T> {
-    loading: boolean;
-    error?: Error | any;
-    value?: T;
-}
+export declare type AsyncState<T> = {
+    loading: true;
+    error?: undefined;
+    value?: undefined;
+} | {
+    loading: false;
+    error: Error;
+    value?: undefined;
+} | {
+    loading: false;
+    error?: undefined;
+    value: T;
+};
 declare const useAsync: <T>(fn: () => Promise<T>, args?: any) => AsyncState<T>;
 export default useAsync;
--- lib/useRenderProp.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-declare const useRenderProp: (element: React.ReactElement<any>) => [React.ReactElement<any>, any[]];
-export default useRenderProp;
