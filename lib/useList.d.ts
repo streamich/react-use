59dd85574e3433de021099d25fc434f716b71210
streamich <streamich@gmail.com>
2019-02-07 11:58:29 +0000

    Updates

--- lib/useList.d.ts
+++ lib/useList.d.ts
@@ -1,5 +1,6 @@
 export interface Actions<T> {
     set: (list: T[]) => void;
+    updateAt: (index: number, item: T) => void;
     push: (item: T) => void;
     filter: (fn: (value: T) => boolean) => void;
     sort: (fn?: (a: T, b: T) => number) => void;
