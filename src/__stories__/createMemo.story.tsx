9c8e093155fdb959b72ca1e3e67bfe061fca7bdf
chengruilin <chengruilin@codemao.cn>
2019-03-08 11:15:32 +0800

    fix: correct createMemo.story

--- src/__stories__/createMemo.story.tsx
+++ src/__stories__/createMemo.story.tsx
@@ -4,8 +4,8 @@ import {createMemo} from '..';
 import ShowDocs from '../util/ShowDocs';
 
 const fibonacci = n => {
-  if (n === 0) return 1;
-  if (n === 1) return 2;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
