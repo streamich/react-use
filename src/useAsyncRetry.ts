7ceff990aac542bae215c15b284f5c9ebdd09838
lvl99 <matt@lvl99.com>
2019-03-20 15:44:55 +0100

    added console message to notify user about useAsyncRetry operation still loading

--- src/useAsyncRetry.ts
+++ src/useAsyncRetry.ts
@@ -8,6 +8,9 @@ const useAsyncRetry = <T>(fn: () => Promise<T>, args: any[] = []) => {
 
   const retry = useCallback(() => {
     if (state.loading) {
+      if (process.env.NODE_ENV !== 'development') {
+        console.log('You are calling useAsyncRetry hook retry() method while loading in progress, this is a no-op.');
+      }
       return;
     }
     setAttempt(attempt + 1);
