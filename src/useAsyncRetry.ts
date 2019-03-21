8e3de1cdf1e287ada1f7baf19679fdf85abcc8a8
Va Da <streamich@users.noreply.github.com>
2019-03-21 10:15:27 +0100

    perf: remove asyn/await wrapper

--- src/useAsyncRetry.ts
+++ src/useAsyncRetry.ts
@@ -3,7 +3,7 @@ import useAsync from './useAsync';
 
 const useAsyncRetry = <T>(fn: () => Promise<T>, args: any[] = []) => {
   const [attempt, setAttempt] = useState<number>(0);
-  const memoized = useCallback(async () => await fn(), [...args, attempt]);
+  const memoized = useCallback(() => fn(), [...args, attempt]);
   const state = useAsync(memoized);
 
   const retry = useCallback(() => {
