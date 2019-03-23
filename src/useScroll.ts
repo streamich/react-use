584bd7e618912fb5617bee33167f7e1b3ce6c176
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-03-23 20:00:43 +1100

    cancel animation frame on un-mount

--- src/useScroll.ts
+++ src/useScroll.ts
@@ -11,35 +11,38 @@ const useScroll = (ref): State => {
   const [state, setState] = useState<State>({
     x: isClient ? window.scrollX : 0,
     y: isClient ? window.scrollY : 0
-  })
+  });
 
   useEffect(() => {
     const handler = () => {
-      cancelAnimationFrame(frame.current)
 
       frame.current = requestAnimationFrame(() => {
         setState({
           x: ref.current.scrollLeft,
           y: ref.current.scrollTop
-        })
-      })
+        });
+      });
     }
 
     if (ref && ref.current) {
       ref.current.addEventListener('scroll', handler, {
         capture: false,
         passive: true
-      })
+      });
     }
 
     return () => {
+      if (frame.current) {
+        cancelAnimationFrame(frame.current);
+      }
+
       if (ref && ref.current) {
-        ref.current.removeEventListener('scroll', handler)
+        ref.current.removeEventListener('scroll', handler);
       }
     };
-  }, [ref])
+  }, [ref]);
 
-  return state
+  return state;
 }
 
 export default useScroll
