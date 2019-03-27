6655092caed4d60f0bb23cc954cd35d63f1bec2f
streamich <streamich@gmail.com>
2019-03-27 19:21:13 +0100

    fix: 🐛 make sure all paths in usePageLeave return

--- src/usePageLeave.ts
+++ src/usePageLeave.ts
@@ -2,18 +2,18 @@ import {useEffect} from 'react';
 
 const usePageLeave = (onPageLeave, args = []) => {
   useEffect(() => {
-    if (onPageLeave) {
-      const handler = (event) => {
-        event = event ? event : (window.event as any);
-        const from = event.relatedTarget || event.toElement;
-        if (!from || (from as any).nodeName === 'HTML') onPageLeave();
-      };
+    if (!onPageLeave) return;
 
-      document.addEventListener('mouseout', handler);
-      return () => {
-        document.removeEventListener('mouseout', handler);
-      };
-    }
+    const handler = (event) => {
+      event = event ? event : (window.event as any);
+      const from = event.relatedTarget || event.toElement;
+      if (!from || (from as any).nodeName === 'HTML') onPageLeave();
+    };
+
+    document.addEventListener('mouseout', handler);
+    return () => {
+      document.removeEventListener('mouseout', handler);
+    };
   }, args);
 };
 
