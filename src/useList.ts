b20cf7c18ad3ce9c939f3afc6e0e47ba68e3304a
streamich <streamich@gmail.com>
2019-03-28 15:14:31 +0100

    feat: 🎸 add clear() to useList, use fn for state updates

--- src/useList.ts
+++ src/useList.ts
@@ -2,6 +2,7 @@ import {useState} from 'react';
 
 export interface Actions<T> {
   set: (list: T[]) => void;
+  clear: () => void;
   updateAt: (index: number, item: T) => void;
   remove: (index: number) => void;
   push: (item: T) => void;
@@ -14,18 +15,19 @@ const useList = <T>(initialList: T[] = []): [T[], Actions<T>] => {
 
   return [list, {
     set,
-    updateAt: (index, entry) => set([
+    clear: () => set([]),
+    updateAt: (index, entry) => set(list => [
       ...list.slice(0, index),
       entry,
       ...list.slice(index + 1)
     ]),
-    remove: (index) => set([
+    remove: (index) => set(list => [
       ...list.slice(0, index),
       ...list.slice(index + 1)
     ]),
-    push: (entry) => set([...list, entry]),
-    filter: (fn) => set(list.filter(fn)),
-    sort: (fn?) => set([...list].sort(fn)),
+    push: (entry) => set(list => [...list, entry]),
+    filter: (fn) => set(list => list.filter(fn)),
+    sort: (fn?) => set(list => [...list].sort(fn)),
   }];
 };
 
