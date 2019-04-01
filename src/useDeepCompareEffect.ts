33b22a718c4e10b68eb9eed20e773d069ee91e98
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-04-01 20:38:13 +1100

    dev warnings as console.warn

--- src/useDeepCompareEffect.ts
+++ src/useDeepCompareEffect.ts
@@ -6,14 +6,14 @@ const isPrimitive = (val: any) => val !== Object(val)
 const useDeepCompareEffect = (effect: EffectCallback, deps: any[]) => {
   if (process.env.NODE_ENV !== 'production') {
     if (!deps || !deps.length) {
-      throw new Error(
-        'useDeepCompareEffect should not be used with no dependencies. Use React.useEffect instead.',
-      )
+      console.warn(
+        '`useDeepCompareEffect` should not be used with no dependencies. Use React.useEffect instead.'
+      );
     }
 
     if (deps.every(isPrimitive)) {
-      throw new Error(
-        'useDeepCompareEffect should not be used with dependencies that are all primitive values. Use React.useEffect instead.',
+      console.warn(
+        '`useDeepCompareEffect` should not be used with dependencies that are all primitive values. Use React.useEffect instead.'
       );
     }
   }
