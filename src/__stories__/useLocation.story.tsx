6f6030a75ed3eb26b4231ff37a5a5bc6ef5fc9ed
streamich <streamich@gmail.com>
2019-04-09 15:49:49 +0200

    fix: 🐛 make useLocation work on server, improve hook

--- src/__stories__/useLocation.story.tsx
+++ src/__stories__/useLocation.story.tsx
@@ -3,13 +3,19 @@ import {storiesOf} from '@storybook/react';
 import {useLocation} from '..';
 import ShowDocs from './util/ShowDocs';
 
+const go = (page) => history.pushState({}, '', page);
+
 const Demo = () => {
   const state = useLocation();
 
   return (
-    <pre>
-      {JSON.stringify(state, null, 2)}
-    </pre>
+    <div>
+      <button onClick={() => go('page-1')}>Page 1</button>
+      <button onClick={() => go('page-2')}>Page 2</button>
+      <pre>
+        {JSON.stringify(state, null, 2)}
+      </pre>
+    </div>
   );
 };
 
