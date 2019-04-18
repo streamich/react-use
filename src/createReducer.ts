32785fc3656bcf635fac27ff1f3fadb82b8559ab
areschen <ariesjia00@hotmail.com>
2019-04-18 15:32:40 +0800

    fix: fix middleware chain issue

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -1,19 +1,28 @@
 import { useCallback, useMemo, useReducer } from 'react';
 
-function applyMiddleware(chain) {
-  return store => {
+function applyMiddleware(chain, store) {
+  return dispatch => {
     return chain.reduceRight((res, middleware) => {
       return middleware(store)(res);
-    }, store.dispatch);
+    }, dispatch);
   };
 }
 
 const createReducer = (...middlewares) => (reducer, initialState, initializer) => {
   const [state, dispatch] = useReducer(reducer, initialState, initializer);
   const getState = useCallback(() => state, [state])
-  const middlewareDispatch = useMemo(() => {
-    const store = { dispatch, getState }
-    return applyMiddleware(middlewares)(store);
+  let middlewareDispatch = () => {
+    throw new Error(
+      'Dispatching while constructing your middleware is not allowed. ' +
+        'Other middleware would not be applied to this dispatch.'
+    );
+  };
+  middlewareDispatch = useMemo(() => {
+    const store = {
+      getState,
+      dispatch: (...args) => middlewareDispatch(...args),
+    };
+    return applyMiddleware(middlewares, store)(dispatch);
   }, middlewares)
 
   return [state, middlewareDispatch];
