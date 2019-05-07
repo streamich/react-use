fa75954a0a267220386e1b20101b399b04eebce6
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-05-07 20:31:22 +1000

    usePrevious hook and story

---
+++ src/__stories__/usePrevious.story.tsx
@@ -0,0 +1,23 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { usePrevious } from '..';
+import ShowDocs from './util/ShowDocs';
+
+const Demo = () => {
+  const [count, setCount] = React.useState(0);
+  const prevCount = usePrevious(count);
+
+  return (
+    <div>
+      <p>
+        Now: {count}, before: {String(prevCount)}
+      </p>
+      <button onClick={() => setCount(value => value + 1)}>+</button>
+      <button onClick={() => setCount(value => value - 1)}>-</button>
+    </div>
+  );
+};
+
+storiesOf('State|usePrevious', module)
+  // .add('Docs', () => <ShowDocs md={require('../../docs/usePrevious.md')} />)
+  .add('Demo', () => <Demo />);
---
+++ src/usePrevious.ts
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from 'react';
+
+const usePrevious = <T>(state: T): T | undefined => {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = state;
+  });
+
+  return ref.current;
+};
+
+export default usePrevious;
