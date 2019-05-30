8f3e31d2f4e5b2140ba2eb52350b433eb66d0cca
bob <bob@mediot.com.tw>
2019-05-30 11:28:01 +0800

    Fixed useSetState definition

--- src/useSetState.ts
+++ src/useSetState.ts
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
-const useSetState = <T extends object>(initialState: T = {} as T): [T, (patch: Partial<T> | (() => void)) => void] => {
+const useSetState = <T extends object>(
+  initialState: T = {} as T
+): [T, (patch: Partial<T> | ((prevState: T) => void)) => void] => {
   const [state, set] = useState<T>(initialState);
   const setState = patch => {
     set(prevState => Object.assign({}, prevState, patch instanceof Function ? patch(prevState) : patch));
