ed17aed4bde3cc230fd1b387acce1f67b36fa52b
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-05-31 13:04:25 -0400

    make types better

--- src/useAsyncFn.ts
+++ src/useAsyncFn.ts
@@ -18,11 +18,16 @@ export type AsyncState<T> =
       value: T;
     };
 
+export type AsyncFn<Result = any, Args extends any[] = any[]> = [
+  AsyncState<Result>,
+  (...args: Args | []) => Promise<Result>
+];
+
 export default function useAsyncFn<Result = any, Args extends any[] = any[]>(
   fn: (...args: Args | []) => Promise<Result>,
   deps: DependencyList = [],
   initialState: AsyncState<Result> = { loading: false }
-): [AsyncState<Result>, (...args: Args | []) => Promise<Result>] {
+): AsyncFn<Result, Args> {
   const [state, set] = useState<AsyncState<Result>>(initialState);
 
   const mounted = useRefMounted();
