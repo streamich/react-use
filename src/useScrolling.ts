bd9928e6b393a5f70e078fe664dbe8cef3e98ac1
James <james@thewebworkers.co.uk>
2019-06-11 10:47:04 +0100

    feat: 🎸 add useScrolling hook

---
+++ src/useScrolling.ts
@@ -0,0 +1,31 @@
+import { RefObject, useEffect, useState } from 'react';
+
+const useScrolling = (ref: RefObject<HTMLElement>): boolean => {
+  const [scrolling, setScrolling] = useState<boolean>(false);
+
+  useEffect(
+    () => {
+      if (ref.current) {
+        let scrollingTimeout;
+
+        const handleScrollEnd = () => {
+          setScrolling(false);
+        };
+
+        const handleScroll = () => {
+          setScrolling(true);
+          clearTimeout(scrollingTimeout);
+          scrollingTimeout = setTimeout(() => handleScrollEnd(), 150);
+        };
+
+        ref.current.addEventListener('scroll', handleScroll, false);
+        return () => ref.current.removeEventListener('scroll', handleScroll, false);
+      }
+    },
+    [ref.current],
+  );
+
+  return scrolling;
+};
+
+export default useScrolling;
