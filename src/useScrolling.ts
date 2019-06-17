d18d2d8683be1a7fc4bf008468ea98b3b5b104de
James <james@thewebworkers.co.uk>
2019-06-17 11:07:02 +0100

    fix: add additional ref check on clean up

--- src/useScrolling.ts
+++ src/useScrolling.ts
@@ -3,27 +3,28 @@ import { RefObject, useEffect, useState } from 'react';
 const useScrolling = (ref: RefObject<HTMLElement>): boolean => {
   const [scrolling, setScrolling] = useState<boolean>(false);
 
-  useEffect(
-    () => {
-      if (ref.current) {
-        let scrollingTimeout;
+  useEffect(() => {
+    if (ref.current) {
+      let scrollingTimeout;
 
-        const handleScrollEnd = () => {
-          setScrolling(false);
-        };
+      const handleScrollEnd = () => {
+        setScrolling(false);
+      };
 
-        const handleScroll = () => {
-          setScrolling(true);
-          clearTimeout(scrollingTimeout);
-          scrollingTimeout = setTimeout(() => handleScrollEnd(), 150);
-        };
+      const handleScroll = () => {
+        setScrolling(true);
+        clearTimeout(scrollingTimeout);
+        scrollingTimeout = setTimeout(() => handleScrollEnd(), 150);
+      };
 
-        ref.current.addEventListener('scroll', handleScroll, false);
-        return () => ref.current.removeEventListener('scroll', handleScroll, false);
-      }
-    },
-    [ref.current],
-  );
+      ref.current.addEventListener('scroll', handleScroll, false);
+      return () => {
+        if (ref.current) {
+          ref.current.removeEventListener('scroll', handleScroll, false);
+        }
+      };
+    }
+  }, [ref.current]);
 
   return scrolling;
 };
