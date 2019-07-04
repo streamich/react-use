4a489d640339cbd3f210316f9979566930eb0fe6
sunxing102005 <39522118+sunxing102005@users.noreply.github.com>
2019-07-04 16:07:15 +0800

    Update useAsync.ts
    add arguments to async function

--- src/useAsync.ts
+++ src/useAsync.ts
@@ -5,14 +5,15 @@ export { AsyncState, AsyncFn } from './useAsyncFn';
 
 export default function useAsync<Result = any, Args extends any[] = any[]>(
   fn: (...args: Args | []) => Promise<Result>,
-  deps: DependencyList = []
+  deps: DependencyList = [],
+  ...callbackArgs: Args | []
 ) {
   const [state, callback] = useAsyncFn<Result, Args>(fn, deps, {
     loading: true,
   });
 
   useEffect(() => {
-    callback();
+    callback(...callbackArgs);
   }, [callback]);
 
   return state;
