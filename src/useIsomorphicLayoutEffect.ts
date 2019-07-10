192c9838b3fee822fdee10c25173ed504bf43101
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-07-10 20:47:52 +1000

    Export useIsomorphicLayoutEffect + docs

--- src/useIsomorphicLayoutEffect.ts
+++ src/useIsomorphicLayoutEffect.ts
@@ -1,9 +1,5 @@
 import { useEffect, useLayoutEffect } from 'react';
 
-/**
- * `useLayoutEffect` that does not show warning on server.
- * See: https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a
- */
 const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
 export default useIsomorphicLayoutEffect;
