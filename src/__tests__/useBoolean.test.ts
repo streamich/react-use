7b2f877dd3b76367bc4ce5b1e8b9a44cc5bb24fb
Mario Beltrán Alarcón <belco90@gmail.com>
2019-07-21 19:55:29 +0200

    Wrap tests in describe block

--- src/__tests__/useBoolean.test.ts
+++ src/__tests__/useBoolean.test.ts
@@ -1,6 +1,8 @@
 import useBoolean from '../useBoolean';
 import useToggle from '../useToggle';
 
-it('should be an alias for useToggle ', () => {
-  expect(useBoolean).toBe(useToggle);
+describe('useBoolean hook', () => {
+  it('should be an alias for useToggle ', () => {
+    expect(useBoolean).toBe(useToggle);
+  });
 });
