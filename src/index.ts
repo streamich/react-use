ca00d64f6151ff8334ca52182fdd747f70ebca3c
xobotyi <xog3@yandex.ru>
2019-07-31 13:48:47 +0300

    Much shortened and simplified the index file by directly reexporting hooks;

--- src/index.ts
+++ src/index.ts
@@ -1,162 +1,81 @@
-import createMemo from './createMemo';
-import createReducer from './createReducer';
-import useAsync from './useAsync';
-import useAsyncFn from './useAsyncFn';
-import useAsyncRetry from './useAsyncRetry';
-import useAudio from './useAudio';
-import useBattery from './useBattery';
-import useBeforeUnload from './useBeforeUnload';
-import useBoolean from './useBoolean';
-import useClickAway from './useClickAway';
-import useCopyToClipboard from './useCopyToClipboard';
-import useCounter from './useCounter';
-import useCss from './useCss';
-import useDebounce from './useDebounce';
-import useDeepCompareEffect from './useDeepCompareEffect';
-import useDefault from './useDefault';
-import useDrop from './useDrop';
-import useDropArea from './useDropArea';
-import useEffectOnce from './useEffectOnce';
-import useEvent from './useEvent';
-import useFavicon from './useFavicon';
-import useFullscreen from './useFullscreen';
-import useGeolocation from './useGeolocation';
-import useGetSet from './useGetSet';
-import useGetSetState from './useGetSetState';
-import useHover from './useHover';
-import useHoverDirty from './useHoverDirty';
-import useIdle from './useIdle';
-import useInterval from './useInterval';
-import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
-import useKey from './useKey';
+export { default as createMemo } from './createMemo';
+export { default as createReducer } from './createReducer';
+export { default as useAsync } from './useAsync';
+export { default as useAsyncFn } from './useAsyncFn';
+export { default as useAsyncRetry } from './useAsyncRetry';
+export { default as useAudio } from './useAudio';
+export { default as useBattery } from './useBattery';
+export { default as useBeforeUnload } from './useBeforeUnload';
+export { default as useBoolean } from './useBoolean';
+export { default as useClickAway } from './useClickAway';
+export { default as useCopyToClipboard } from './useCopyToClipboard';
+export { default as useCounter } from './useCounter';
+export { default as useCss } from './useCss';
+export { default as useDebounce } from './useDebounce';
+export { default as useDeepCompareEffect } from './useDeepCompareEffect';
+export { default as useDefault } from './useDefault';
+export { default as useDrop } from './useDrop';
+export { default as useDropArea } from './useDropArea';
+export { default as useEffectOnce } from './useEffectOnce';
+export { default as useEvent } from './useEvent';
+export { default as useFavicon } from './useFavicon';
+export { default as useFullscreen } from './useFullscreen';
+export { default as useGeolocation } from './useGeolocation';
+export { default as useGetSet } from './useGetSet';
+export { default as useGetSetState } from './useGetSetState';
+export { default as useHover } from './useHover';
+export { default as useHoverDirty } from './useHoverDirty';
+export { default as useIdle } from './useIdle';
+export { default as useInterval } from './useInterval';
+export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
+export { default as useKey } from './useKey';
 // not exported because of peer dependency
-// import useKeyboardJs from './useKeyboardJs';
-import useKeyPress from './useKeyPress';
-import useKeyPressEvent from './useKeyPressEvent';
-import useLifecycles from './useLifecycles';
-import useList from './useList';
-import useLocalStorage from './useLocalStorage';
-import useLocation from './useLocation';
-import useLockBodyScroll from './useLockBodyScroll';
-import useLogger from './useLogger';
-import useMap from './useMap';
-import useMedia from './useMedia';
-import useMediaDevices from './useMediaDevices';
-import useMotion from './useMotion';
-import useMount from './useMount';
-import useMouse from './useMouse';
-import useMouseHovered from './useMouseHovered';
-import useNetwork from './useNetwork';
-import useNumber from './useNumber';
-import useObservable from './useObservable';
-import useOrientation from './useOrientation';
-import usePageLeave from './usePageLeave';
-import usePermission from './usePermission';
-import usePrevious from './usePrevious';
-import usePromise from './usePromise';
-import useRaf from './useRaf';
-import useRafLoop from './useRafLoop';
-import useRefMounted from './useRefMounted';
-import useScroll from './useScroll';
-import useScrolling from './useScrolling';
-import useSessionStorage from './useSessionStorage';
-import useSetState from './useSetState';
-import useSize from './useSize';
-import useSpeech from './useSpeech';
+// export { default as useKeyboardJs } from './useKeyboardJs';
+export { default as useKeyPress } from './useKeyPress';
+export { default as useKeyPressEvent } from './useKeyPressEvent';
+export { default as useLifecycles } from './useLifecycles';
+export { default as useList } from './useList';
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useLocation } from './useLocation';
+export { default as useLockBodyScroll } from './useLockBodyScroll';
+export { default as useLogger } from './useLogger';
+export { default as useMap } from './useMap';
+export { default as useMedia } from './useMedia';
+export { default as useMediaDevices } from './useMediaDevices';
+export { default as useMotion } from './useMotion';
+export { default as useMount } from './useMount';
+export { default as useMouse } from './useMouse';
+export { default as useMouseHovered } from './useMouseHovered';
+export { default as useNetwork } from './useNetwork';
+export { default as useNumber } from './useNumber';
+export { default as useObservable } from './useObservable';
+export { default as useOrientation } from './useOrientation';
+export { default as usePageLeave } from './usePageLeave';
+export { default as usePermission } from './usePermission';
+export { default as usePrevious } from './usePrevious';
+export { default as usePromise } from './usePromise';
+export { default as useRaf } from './useRaf';
+export { default as useRafLoop } from './useRafLoop';
+export { default as useRefMounted } from './useRefMounted';
+export { default as useScroll } from './useScroll';
+export { default as useScrolling } from './useScrolling';
+export { default as useSessionStorage } from './useSessionStorage';
+export { default as useSetState } from './useSetState';
+export { default as useSize } from './useSize';
+export { default as useSpeech } from './useSpeech';
 // not exported because of peer dependency
-// import useSpring from './useSpring';
-import useStartTyping from './useStartTyping';
-import useThrottle from './useThrottle';
-import useThrottleFn from './useThrottleFn';
-import useTimeout from './useTimeout';
-import useTitle from './useTitle';
-import useToggle from './useToggle';
-import useTween from './useTween';
-import useUnmount from './useUnmount';
-import useUpdate from './useUpdate';
-import useUpdateEffect from './useUpdateEffect';
-import useVideo from './useVideo';
-import { useWait, Waiter } from './useWait';
-import useWindowScroll from './useWindowScroll';
-import useWindowSize from './useWindowSize';
-
-export {
-  createMemo,
-  createReducer,
-  useAsync,
-  useAsyncFn,
-  useAsyncRetry,
-  useAudio,
-  useBattery,
-  useBeforeUnload,
-  useBoolean,
-  useClickAway,
-  useCopyToClipboard,
-  useCounter,
-  useCss,
-  useDebounce,
-  useDeepCompareEffect,
-  useDefault,
-  useDrop,
-  useDropArea,
-  useEffectOnce,
-  useEvent,
-  useFavicon,
-  useFullscreen,
-  useGeolocation,
-  useGetSet,
-  useGetSetState,
-  useHover,
-  useHoverDirty,
-  useIdle,
-  useInterval,
-  useIsomorphicLayoutEffect,
-  useKey,
-  useKeyPress,
-  useKeyPressEvent,
-  useLifecycles,
-  useList,
-  useLocalStorage,
-  useLocation,
-  useLockBodyScroll,
-  useLogger,
-  useMap,
-  useMedia,
-  useMediaDevices,
-  useMotion,
-  useMount,
-  useMouse,
-  useMouseHovered,
-  useNetwork,
-  useNumber,
-  useObservable,
-  useOrientation,
-  usePageLeave,
-  usePermission,
-  usePrevious,
-  usePromise,
-  useRaf,
-  useRafLoop,
-  useRefMounted,
-  useScroll,
-  useScrolling,
-  useSessionStorage,
-  useSetState,
-  useSize,
-  useSpeech,
-  useStartTyping,
-  useThrottle,
-  useThrottleFn,
-  useTimeout,
-  useTitle,
-  useToggle,
-  useTween,
-  useUnmount,
-  useUpdate,
-  useUpdateEffect,
-  useVideo,
-  useWait,
-  useWindowScroll,
-  useWindowSize,
-  Waiter,
-};
+// export { default as useSpring } from './useSpring';
+export { default as useStartTyping } from './useStartTyping';
+export { default as useThrottle } from './useThrottle';
+export { default as useThrottleFn } from './useThrottleFn';
+export { default as useTimeout } from './useTimeout';
+export { default as useTitle } from './useTitle';
+export { default as useToggle } from './useToggle';
+export { default as useTween } from './useTween';
+export { default as useUnmount } from './useUnmount';
+export { default as useUpdate } from './useUpdate';
+export { default as useUpdateEffect } from './useUpdateEffect';
+export { default as useVideo } from './useVideo';
+export { useWait, Waiter } from './useWait';
+export { default as useWindowScroll } from './useWindowScroll';
+export { default as useWindowSize } from './useWindowSize';
