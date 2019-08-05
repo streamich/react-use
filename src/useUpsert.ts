8462a59e16b7d650517f3ac76c736bdb43988d08
petterive <iversen332@gmail.com>
2019-08-05 23:18:07 +0200

    refactor: 💡 rename comparison function

--- src/useUpsert.ts
+++ src/useUpsert.ts
@@ -5,17 +5,17 @@ export interface Actions<T> extends ListActions<T> {
 }
 
 const useUpsert = <T>(
-  itemsAreTheSame: (upsertedItem: T, existingItem: T) => boolean,
+  comparisonFunction: (upsertedItem: T, existingItem: T) => boolean,
   initialList: T[] = []
 ): [T[], Actions<T>] => {
   const [items, actions] = useList(initialList);
 
   const upsert = (upsertedItem: T) => {
-    const itemAlreadyExists = items.find(item => itemsAreTheSame(upsertedItem, item));
+    const itemAlreadyExists = items.find(item => comparisonFunction(upsertedItem, item));
     if (itemAlreadyExists) {
       return actions.set(
         items.map(existingItem => {
-          if (itemsAreTheSame(upsertedItem, existingItem)) {
+          if (comparisonFunction(upsertedItem, existingItem)) {
             return upsertedItem;
           }
           return existingItem;
