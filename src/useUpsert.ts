6875e13d60cbbb3e94c19e55a10d866be0d5713f
petterive <iversen332@gmail.com>
2019-08-05 22:51:05 +0200

    feat: 🎸 add useUpsert

---
+++ src/useUpsert.ts
@@ -0,0 +1,37 @@
+import useList, { Actions as ListActions } from './useList';
+
+export interface Actions<T> extends ListActions<T> {
+  upsert: (item: T) => void;
+}
+
+const useUpsert = <T>(
+  itemsAreTheSame: (upsertedItem: T, existingItem: T) => boolean,
+  initialList: T[] = []
+): [T[], Actions<T>] => {
+  const [items, actions] = useList(initialList);
+
+  const upsert = (upsertedItem: T) => {
+    const itemAlreadyExists = items.find(item => itemsAreTheSame(upsertedItem, item));
+    if (itemAlreadyExists) {
+      return actions.set(
+        items.map(existingItem => {
+          if (itemsAreTheSame(upsertedItem, existingItem)) {
+            return upsertedItem;
+          }
+          return existingItem;
+        })
+      );
+    }
+    return actions.push(upsertedItem);
+  };
+
+  return [
+    items,
+    {
+      ...actions,
+      upsert,
+    },
+  ];
+};
+
+export default useUpsert;
