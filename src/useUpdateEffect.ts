1d5cd10fe4d031cb2c2daa0b44b8273fbe2f4040
砖家 <576679268@qq.com>
2019-08-12 23:39:59 +0800

    fix: useUpdateEffect run on the wrong time

--- src/useUpdateEffect.ts
+++ src/useUpdateEffect.ts
@@ -3,14 +3,13 @@ import { useEffect, useRef } from 'react';
 const useUpdateEffect: typeof useEffect = (effect, deps) => {
   const isInitialMount = useRef(true);
 
-  useEffect(
-    isInitialMount.current
-      ? () => {
-          isInitialMount.current = false;
-        }
-      : effect,
-    deps
-  );
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      effect();
+    }
+  }, deps);
 };
 
 export default useUpdateEffect;
