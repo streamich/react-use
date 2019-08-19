f823aaed8a9aa9e6f81ee145de77e159d0ae4557
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-08-19 20:52:03 +1000

    Improve useWindowSize performance with requestAnimationFrame

--- src/useWindowSize.ts
+++ src/useWindowSize.ts
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { isClient } from './util';
 
 const useWindowSize = (initialWidth = Infinity, initialHeight = Infinity) => {
+  const frame = useRef(0);
   const [state, setState] = useState<{ width: number; height: number }>({
     width: isClient ? window.innerWidth : initialWidth,
     height: isClient ? window.innerHeight : initialHeight,
@@ -10,13 +11,23 @@ const useWindowSize = (initialWidth = Infinity, initialHeight = Infinity) => {
   useEffect(() => {
     if (isClient) {
       const handler = () => {
-        setState({
-          width: window.innerWidth,
-          height: window.innerHeight,
+        cancelAnimationFrame(frame.current);
+
+        frame.current = requestAnimationFrame(() => {
+          setState({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
         });
       };
+
       window.addEventListener('resize', handler);
-      return () => window.removeEventListener('resize', handler);
+
+      return () => {
+        cancelAnimationFrame(frame.current);
+
+        window.removeEventListener('resize', handler);
+      };
     } else {
       return undefined;
     }
