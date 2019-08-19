693aec8307c378697c88c635a401832f24d3531a
尽龙 <brickspert.fjl@antfin.com>
2019-08-19 14:18:40 +0800

    feat: useDebounce add cancel

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,13 +1,9 @@
 import { useCallback, useRef } from 'react';
 import useUpdateEffect from './useUpdateEffect';
 
-
 const useDebounce = (fn: () => any, ms: number = 0, args: any[] = []) => {
-
   const timer = useRef<number>();
-
   useUpdateEffect(() => {
-
     timer.current = window.setTimeout(fn.bind(null, args), ms);
     return () => {
       // if args change then clear timeout
