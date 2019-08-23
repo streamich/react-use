2617d74b8ad5b14960b550b3ea6f03e6fdcba4c0
xobotyi <xog3@yandex.ru>
2019-08-24 02:14:26 +0300

    fix: #550

--- src/__stories__/useLockBodyScroll.story.tsx
+++ src/__stories__/useLockBodyScroll.story.tsx
@@ -1,5 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
+import { useRef } from 'react';
+import Frame from 'react-frame-component';
 import { useLockBodyScroll, useToggle } from '..';
 import ShowDocs from './util/ShowDocs';
 
@@ -27,6 +29,30 @@ const AnotherComponent = () => {
   );
 };
 
+const IframeComponent = () => {
+  const [mainLocked, toggleMainLocked] = useToggle(false);
+  const [iframeLocked, toggleIframeLocked] = useToggle(false);
+  const iframeElementRef = useRef<HTMLIFrameElement>(null);
+
+  useLockBodyScroll(mainLocked);
+  useLockBodyScroll(iframeLocked, iframeElementRef);
+
+  return (
+    <div style={{ height: '200vh' }}>
+      <Frame style={{ height: '50vh', width: '50vw' }}>
+        <div style={{ height: '200vh' }} ref={iframeElementRef}>
+          <button onClick={() => toggleMainLocked()} style={{ position: 'fixed', left: 0, top: 0 }}>
+            {mainLocked ? 'Unlock' : 'Lock'} main window scroll
+          </button>
+          <button onClick={() => toggleIframeLocked()} style={{ position: 'fixed', left: 0, top: 64 }}>
+            {iframeLocked ? 'Unlock' : 'Lock'} iframe window scroll
+          </button>
+        </div>
+      </Frame>
+    </div>
+  );
+};
+
 storiesOf('Side effects|useLockBodyScroll', module)
   .add('Docs', () => <ShowDocs md={require('../../docs/useLockBodyScroll.md')} />)
   .add('Demo', () => <Demo />)
@@ -34,5 +60,7 @@ storiesOf('Side effects|useLockBodyScroll', module)
     <>
       <AnotherComponent />
       <Demo />
+      <IframeComponent />
     </>
-  ));
+  ))
+  .add('Iframe', () => <IframeComponent />);
--- src/useLockBodyScroll.ts
+++ src/useLockBodyScroll.ts
@@ -1,34 +1,58 @@
-import { useEffect } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 
-let counter = 0;
-let originalOverflow: string | null = null;
-
-const lock = () => {
-  originalOverflow = window.getComputedStyle(document.body).overflow;
-  document.body.style.overflow = 'hidden';
-};
-
-const unlock = () => {
-  document.body.style.overflow = originalOverflow;
-  originalOverflow = null;
-};
-
-const increment = () => {
-  counter++;
-  if (counter === 1) {
-    lock();
+export function getClosestBody(el: Element | HTMLElement | HTMLIFrameElement | null): HTMLElement | null {
+  if (!el) {
+    return null;
+  } else if (el.tagName === 'BODY') {
+    return el as HTMLElement;
+  } else if (el.tagName === 'IFRAME') {
+    const document = (el as HTMLIFrameElement).contentDocument;
+    return document ? document.body : null;
+  } else if (!(el as HTMLElement).offsetParent) {
+    return null;
   }
-};
 
-const decrement = () => {
-  counter--;
-  if (counter === 0) {
-    unlock();
-  }
-};
+  return getClosestBody((el as HTMLElement).offsetParent!);
+}
 
-const useLockBodyScroll = (enabled: boolean = true) => {
-  useEffect(() => (enabled ? (increment(), decrement) : undefined), [enabled]);
-};
+export interface BodyInfoItem {
+  counter: number;
+  initialOverflow: string | null;
+}
 
-export default useLockBodyScroll;
+const bodies: Map<HTMLElement, BodyInfoItem> = new Map();
+
+const doc: Document | undefined = typeof document === 'object' ? document : undefined;
+
+export default !doc
+  ? function useLockBodyMock(_locked: boolean = true, _elementRef?: RefObject<HTMLElement>) {}
+  : function useLockBody(locked: boolean = true, elementRef?: RefObject<HTMLElement>) {
+      elementRef = elementRef || useRef(doc!.body);
+
+      useEffect(() => {
+        const body = getClosestBody(elementRef!.current);
+        if (!body) {
+          return;
+        }
+
+        const bodyInfo = bodies.get(body);
+
+        if (locked) {
+          if (!bodyInfo) {
+            bodies.set(body, { counter: 1, initialOverflow: body.style.overflow });
+            body.style.overflow = 'hidden';
+          } else {
+            bodies.set(body, { counter: bodyInfo.counter + 1, initialOverflow: bodyInfo.initialOverflow });
+          }
+        } else {
+          if (bodyInfo) {
+            if (bodyInfo.counter === 1) {
+              bodies.delete(body);
+              body.style.overflow = bodyInfo.initialOverflow;
+            } else {
+              bodies.set(body, { counter: bodyInfo.counter - 1, initialOverflow: bodyInfo.initialOverflow });
+            }
+          }
+        }
+      }, [locked, elementRef.current]);
+    };
