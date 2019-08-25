66f06905819184fcda2d1753182b73faf7788a19
xobotyi <xog3@yandex.ru>
2019-08-26 00:52:57 +0300

    Way simpler with `useTimeoutFn`;

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,23 +1,9 @@
-import { useCallback, useRef } from 'react';
-import useUpdateEffect from './useUpdateEffect';
+import { DependencyList, useCallback } from 'react';
+import useTimeoutFn from './useTimeoutFn';
 
-const useDebounce = (fn: () => any, ms: number = 0, args: any[] = []) => {
-  const timer = useRef<number>();
-  useUpdateEffect(() => {
-    timer.current = window.setTimeout(fn.bind(null, args), ms);
-    return () => {
-      // if args change then clear timeout
-      window.clearTimeout(timer.current);
-    };
-  }, args);
+export default function useDebounce(fn: (...args: any[]) => any, ms: number = 0, args: DependencyList = []) {
+  const cb = useCallback(fn, args);
+  const [, cancel] = useTimeoutFn(cb, ms);
 
-  const cancel = useCallback(() => {
-    if (timer.current) {
-      window.clearTimeout(timer.current);
-    }
-  }, []);
-
-  return { cancel };
-};
-
-export default useDebounce;
+  return [cancel];
+}
