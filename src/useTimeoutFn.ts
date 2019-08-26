438a56606bb90d140425bd9f55e2b8c453f20151
xobotyi <xog3@yandex.ru>
2019-08-26 09:58:28 +0300

    make useTimeoutFn work properly: - reset function should clear previous timeout; - function change should not reset the timeout;

--- src/useTimeoutFn.ts
+++ src/useTimeoutFn.ts
@@ -5,20 +5,31 @@ export type UseTimeoutFnReturn = [() => boolean | null, () => void, () => void];
 export default function useTimeoutFn(fn: Function, ms: number = 0): UseTimeoutFnReturn {
   const ready = useRef<boolean | null>(false);
   const timeout = useRef<ReturnType<typeof setTimeout>>();
+  const callback = useRef(fn);
 
   const isReady = useCallback(() => ready.current, []);
+
   const set = useCallback(() => {
     ready.current = false;
+    timeout.current && clearTimeout(timeout.current);
+
     timeout.current = setTimeout(() => {
       ready.current = true;
-      fn();
+      callback.current();
     }, ms);
-  }, [ms, fn]);
+  }, [ms]);
+
   const clear = useCallback(() => {
     ready.current = null;
     timeout.current && clearTimeout(timeout.current);
   }, []);
 
+  // update ref when function changes
+  useEffect(() => {
+    callback.current = fn;
+  }, [fn]);
+
+  // set on mount, clear on unmount
   useEffect(() => {
     set();
 
