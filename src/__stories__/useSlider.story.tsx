2d97cdfadab56c54741e7871d717d911d60c728f
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-10-17 00:28:30 +1100

    More fun story

--- src/__stories__/useSlider.story.tsx
+++ src/__stories__/useSlider.story.tsx
@@ -9,11 +9,11 @@ const Demo = () => {
 
   return (
     <div>
-      <div ref={ref} style={{ position: 'relative', background: 'lightgray', padding: 4 }}>
-        <p style={{ margin: 0, textAlign: 'center', color: state.isSliding ? 'red' : 'green' }}>
-          {Math.round(state.value * 100)}%
-        </p>
-        <div style={{ position: 'absolute', top: 0, left: state.value * state.length - 10 }}>🎚</div>
+      <div ref={ref} style={{ position: 'relative', background: 'yellow', padding: 4 }}>
+        <p style={{ margin: 0, textAlign: 'center' }}>Slide me</p>
+        <div style={{ position: 'absolute', top: 0, left: state.pos, transform: 'scale(2)' }}>
+          {state.isSliding ? '🏂' : '🎿'}
+        </div>
       </div>
       <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
