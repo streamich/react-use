7ac2f9b0dc384609ba6a61a3894b402e2c112df5
xobotyi <xog3@yandex.ru>
2019-10-26 12:47:06 +0300

    impl(useStateHistory): added initialHistory parameter and go(pos: number) method; impl(useStateHistory): improved story demo; impl(useStateHistory): docs;

--- src/__stories__/useStateHistory.story.tsx
+++ src/__stories__/useStateHistory.story.tsx
@@ -1,28 +1,57 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { useCounter, useStateHistory } from '..';
 import ShowDocs from './util/ShowDocs';
 
 const Demo = () => {
-  const [state, setState, history] = useStateHistory('');
-  const input = useRef<HTMLInputElement | null>(null);
+  const [state, setState, history] = useStateHistory('', ['hello', 'world']);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const [stepSize, { set: setStepSize }] = useCounter(1, 3, 1);
 
-  console.log(history);
+  const handleFormSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      state !== inputRef.current!.value && setState(inputRef.current!.value);
+    },
+    [state]
+  );
+
+  const handleBackClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (e.currentTarget.disabled) {
+        return;
+      }
+
+      history.back(stepSize);
+    },
+    [history, stepSize]
+  );
+
+  const handleForwardClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (e.currentTarget.disabled) {
+        return;
+      }
+
+      history.forward(stepSize);
+    },
+    [history, stepSize]
+  );
+
+  const handleStepSizeChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setStepSize((e.currentTarget.value as any) * 1);
+    },
+    [stepSize]
+  );
 
   return (
     <div>
       <div>
-        <form
-          onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            state !== input.current!.value && setState(input.current!.value);
-          }}
-          style={{ display: 'inline-block' }}
-        >
-          <input type="text" ref={input} />
+        <form onSubmit={handleFormSubmit} style={{ display: 'inline-block' }}>
+          <input type="text" ref={inputRef} />
           <button>Submit new state</button>
         </form>
       </div>
@@ -31,38 +60,23 @@ const Demo = () => {
         Current state: <span>{state}</span>
       </div>
       <div style={{ marginTop: 8 }}>
-        <button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-            if (e.currentTarget.disabled) {
-              return;
-            }
-
-            history.back(stepSize);
-          }}
-          disabled={!history.position}
-        >
+        <button onClick={handleBackClick} disabled={!history.position}>
           &lt; Back
         </button>
-        <button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-            if (e.currentTarget.disabled) {
-              return;
-            }
-
-            history.forward(stepSize);
-          }}
-          disabled={history.position >= history.history.length - 1}
-        >
+        <button onClick={handleForwardClick} disabled={history.position >= history.history.length - 1}>
           Forward &gt;
         </button>
         &nbsp; Step size:&nbsp;
-        <input
-          type="number"
-          value={stepSize}
-          min={1}
-          max={3}
-          onChange={(e: React.FormEvent<HTMLInputElement>) => {
-            setStepSize((e.currentTarget.value as any) * 1);
+        <input type="number" value={stepSize} min={1} max={3} onChange={handleStepSizeChange} />
+      </div>
+
+      <div style={{ marginTop: 8 }}>
+        <div>Current history</div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(history.history, null, 2)
+              .replace(/\n/g, '<br/>')
+              .replace(/ /g, '&nbsp;'),
           }}
         />
       </div>
@@ -71,5 +85,5 @@ const Demo = () => {
 };
 
 storiesOf('State|useStateHistory', module)
-  .add('Docs', () => <ShowDocs md={require('../../docs/useMultiStateValidator.md')} />)
+  .add('Docs', () => <ShowDocs md={require('../../docs/useStateHistory.md')} />)
   .add('Demo', () => <Demo />);
