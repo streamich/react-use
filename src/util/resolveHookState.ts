befcf84c2c71a9ff3c83c83c0afe3e733737add3
xobotyi <xog3@yandex.ru>
2019-10-31 04:48:30 +0300

    feat(useCounter): reworked with use of new resolveHookState function plus improved memory usage; feat(resolveHookState): improved types;

--- src/util/resolveHookState.ts
+++ src/util/resolveHookState.ts
@@ -5,14 +5,12 @@ export type InitialHookState<S> = S | InitialStateSetter<S>;
 export type HookState<S> = S | StateSetter<S>;
 export type ResolvableHookState<S> = S | StateSetter<S> | InitialStateSetter<S>;
 
-export function resolveHookState<S>(newState: S | InitialStateSetter<S>): S;
-export function resolveHookState<S>(newState: Exclude<HookState<any>, StateSetter<any>>, currentState: S): S;
-// tslint:disable-next-line:unified-signatures
-export function resolveHookState<S>(newState: StateSetter<S>, currentState: S): S;
-export function resolveHookState<S>(newState: ResolvableHookState<S>, currentState?: S): S {
+export function resolveHookState<S, C extends S>(newState: StateSetter<S>, currentState: C): S;
+export function resolveHookState<S, C extends S>(newState: ResolvableHookState<S>, currentState?: C): S;
+export function resolveHookState<S, C extends S>(newState: ResolvableHookState<S>, currentState?: C): S {
   if (typeof newState === 'function') {
     return (newState as Function)(currentState);
   }
 
-  return newState as S;
+  return newState;
 }
