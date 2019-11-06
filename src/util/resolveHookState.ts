1840b577e2a3d321b8dbb44d5ae443e84d4d9e20
xobotyi <xog3@yandex.ru>
2019-11-06 14:49:33 +0300

    feat(useList): reimplemented useList hook; feat(useList): new action upsert; feat(useList): new action update; feat(useList): new action updateFirst; feat(useList): new action insertAt; feat(useList): action remove renamed to removeAt (ref remained); feat(useUpsert): useUpsert hook deprecated cause of duplicate functionality and bad naming;

--- src/util/resolveHookState.ts
+++ src/util/resolveHookState.ts
@@ -5,9 +5,10 @@ export type InitialHookState<S> = S | InitialStateSetter<S>;
 export type HookState<S> = S | StateSetter<S>;
 export type ResolvableHookState<S> = S | StateSetter<S> | InitialStateSetter<S>;
 
-export function resolveHookState<S, C extends S>(newState: StateSetter<S>, currentState: C): S;
-export function resolveHookState<S, C extends S>(newState: ResolvableHookState<S>, currentState?: C): S;
-export function resolveHookState<S, C extends S>(newState: ResolvableHookState<S>, currentState?: C): S {
+export function resolveHookState<S>(newState: InitialStateSetter<S>): S;
+export function resolveHookState<S>(newState: StateSetter<S>, currentState: S): S;
+export function resolveHookState<S>(newState: ResolvableHookState<S>, currentState?: S): S;
+export function resolveHookState<S>(newState: ResolvableHookState<S>, currentState?: S): S {
   if (typeof newState === 'function') {
     return (newState as Function)(currentState);
   }
