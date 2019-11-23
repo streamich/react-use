30abe2b22e3cb7a3e4c6dedd2466d74ce660911d
Anton Zinovyev <xog3@yandex.ru>
2019-11-23 15:14:35 +0300

    feat: add useFirstMountState & useRendersCount hooks (#769)
    feat(useFirstMountState): hook to track if render is first;
    feat(useRendersCount): hook to track renders count;
    refactor(useUpdateEffect): now uses useFirstMountState hook;
    refactor(usePreviousDistinct): now uses with useFirstMountState hook;
    docs: update readme;

--- src/index.ts
+++ src/index.ts
@@ -94,4 +94,6 @@ export { useMultiStateValidator } from './useMultiStateValidator';
 export { default as useWindowScroll } from './useWindowScroll';
 export { default as useWindowSize } from './useWindowSize';
 export { default as useMeasure } from './useMeasure';
+export { useRendersCount } from './useRendersCount';
+export { useFirstMountState } from './useFirstMountState';
 export { default as useSet } from './useSet';
