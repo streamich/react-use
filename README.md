0a6635914319e9ef7a2902189a3c2dea90a2bf7f
Anton Zinovyev <xog3@yandex.ru>
2019-11-28 16:41:48 +0300

    feat: useStateHistory (#709)
    * feat: add useStateHistory hook;
    * fix: createBreakpoint lint fix (author skipped hooks);

--- README.md
+++ README.md
@@ -137,6 +137,7 @@
   - [`useSet`](./docs/useSet.md) &mdash; tracks state of a Set. [![][img-demo]](https://codesandbox.io/s/bold-shtern-6jlgw)
   - [`useQueue`](./docs/useQueue.md) &mdash; implements simple queue.
   - [`useStateValidator`](./docs/useStateValidator.md) &mdash; tracks state of an object. [![][img-demo]](https://streamich.github.io/react-use/?path=/story/state-usestatevalidator--demo)
+  - [`useStateWithHistory`](./docs/useStateWithHistory.md) &mdash; stores previous state values and provides handles to travel through them. [![][img-demo]](https://streamich.github.io/react-use/?path=/story/state-usestatewithhistory--demo)
   - [`useMultiStateValidator`](./docs/useMultiStateValidator.md) &mdash; alike the `useStateValidator`, but tracks multiple states at a time. [![][img-demo]](https://streamich.github.io/react-use/?path=/story/state-usemultistatevalidator--demo)
   - [`useMediatedState`](./docs/useMediatedState.md) &mdash; like the regular `useState` but with mediation by custom function. [![][img-demo]](https://streamich.github.io/react-use/?path=/story/state-usemediatedstate--demo)
   - [`useFirstMountState`](./docs/useFirstMountState.md) &mdash; check if current render is first. [![][img-demo]](https://streamich.github.io/react-use/?path=/story/state-usefirstmountstate--demo)
---
+++ src/useStateWithHistory.ts
@@ -0,0 +1,131 @@
+import { Dispatch, useCallback, useMemo, useRef, useState } from 'react';
+import { useFirstMountState } from './useFirstMountState';
+import { InitialHookState, ResolvableHookState, resolveHookState } from './util/resolveHookState';
+
+interface HistoryState<S> {
+  history: S[];
+  position: number;
+  capacity: number;
+  back: (amount?: number) => void;
+  forward: (amount?: number) => void;
+  go: (position: number) => void;
+}
+
+export type UseStateHistoryReturn<S> = [S, Dispatch<ResolvableHookState<S>>, HistoryState<S>];
+
+export function useStateWithHistory<S, I extends S>(
+  initialState: InitialHookState<S>,
+  capacity?: number,
+  initialHistory?: I[]
+): UseStateHistoryReturn<S>;
+export function useStateWithHistory<S = undefined>(): UseStateHistoryReturn<S | undefined>;
+
+export function useStateWithHistory<S, I extends S>(
+  initialState?: InitialHookState<S>,
+  capacity: number = 10,
+  initialHistory?: I[]
+): UseStateHistoryReturn<S> {
+  if (capacity < 1) {
+    throw new Error(`Capacity has to be greater than 1, got '${capacity}'`);
+  }
+
+  const isFirstMount = useFirstMountState();
+  const [state, innerSetState] = useState<S>(initialState as S);
+  const history = useRef<S[]>((initialHistory ?? []) as S[]);
+  const historyPosition = useRef(0);
+
+  // do the states manipulation only on first mount, no sense to load re-renders with useless calculations
+  if (isFirstMount) {
+    if (history.current.length) {
+      // if last element of history !== initial - push initial to history
+      if (history.current[history.current.length - 1] !== initialState) {
+        history.current.push(initialState as I);
+      }
+
+      // if initial history bigger that capacity - crop the first elements out
+      if (history.current.length > capacity) {
+        history.current = history.current.slice(history.current.length - capacity);
+      }
+    } else {
+      // initiate the history with initial state
+      history.current.push(initialState as I);
+    }
+
+    historyPosition.current = history.current.length && history.current.length - 1;
+  }
+
+  const setState = useCallback(
+    (newState: ResolvableHookState<S>): void => {
+      innerSetState(currentState => {
+        newState = resolveHookState(newState);
+
+        // is state has changed
+        if (newState !== currentState) {
+          // if current position is not the last - pop element to the right
+          if (historyPosition.current < history.current.length - 1) {
+            history.current = history.current.slice(0, historyPosition.current + 1);
+          }
+
+          historyPosition.current = history.current.push(newState as I) - 1;
+
+          // if capacity is reached - shift first elements
+          if (history.current.length > capacity) {
+            history.current = history.current.slice(history.current.length - capacity);
+          }
+        }
+
+        return newState;
+      });
+    },
+    [state, capacity]
+  ) as Dispatch<ResolvableHookState<S>>;
+
+  const historyState = useMemo(
+    () => ({
+      history: history.current,
+      position: historyPosition.current,
+      capacity,
+      back: (amount: number = 1) => {
+        // don't do anything if we already at the left border
+        if (!historyPosition.current) {
+          return;
+        }
+
+        innerSetState(() => {
+          historyPosition.current -= Math.min(amount, historyPosition.current);
+
+          return history.current[historyPosition.current];
+        });
+      },
+      forward: (amount: number = 1) => {
+        // don't do anything if we already at the right border
+        if (historyPosition.current === history.current.length - 1) {
+          return;
+        }
+
+        innerSetState(() => {
+          historyPosition.current = Math.min(historyPosition.current + amount, history.current.length - 1);
+
+          return history.current[historyPosition.current];
+        });
+      },
+      go: (position: number) => {
+        if (position === historyPosition.current) {
+          return;
+        }
+
+        innerSetState(() => {
+          historyPosition.current =
+            position < 0
+              ? Math.max(history.current.length + position, 0)
+              : Math.min(history.current.length - 1, position);
+
+          return history.current[historyPosition.current];
+        });
+      },
+    }),
+    [state]
+  );
+
+  return [state, setState, historyState];
+}
