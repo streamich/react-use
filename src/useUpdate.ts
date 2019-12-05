93e72910abf2dafe5bdff625a21f633afd6e52c5
Olzhas Alexandrov <olzhas@alexandrov.co>
2019-12-05 17:04:09 -0600

    fix: useUpdate hitting maxInt, failing to trigger rerender
    Signed-off-by: Olzhas Alexandrov <olzhas@alexandrov.co>

--- src/useUpdate.ts
+++ src/useUpdate.ts
@@ -1,9 +1,16 @@
 import { useCallback, useState } from 'react';
 
-const incrementParameter = (num: number): number => ++num;
+/**
+ * MIN & MAX safe integers are literals due to no support in IE
+ */
+const minInt = -1000000000
+const maxInt = 9007199254740991
+const incrementParameter = (num: number): number => {
+  return num !== maxInt ? (num += 1) : minInt;
+}
 
 const useUpdate = () => {
-  const [, setState] = useState(0);
+  const [, setState] = useState(minInt);
   // useCallback with empty deps as we only want to define updateCb once
   return useCallback(() => setState(incrementParameter), []);
 };
