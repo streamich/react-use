587de16ef5c85497d01e63247a578116d0605ff9
suyingtao <suyingtao@baidu.com>
2019-12-13 18:45:01 +0800

    feat(useLocalStorage): add remove feature. (#229)

--- docs/useLocalStorage.md
+++ docs/useLocalStorage.md
@@ -9,13 +9,14 @@ React side-effect hook that manages a single `localStorage` key.
 import {useLocalStorage} from 'react-use';
 
 const Demo = () => {
-  const [value, setValue] = useLocalStorage('my-key', 'foo');
+  const [value, setValue, remove] = useLocalStorage('my-key', 'foo');
 
   return (
     <div>
       <div>Value: {value}</div>
       <button onClick={() => setValue('bar')}>bar</button>
       <button onClick={() => setValue('baz')}>baz</button>
+      <button onClick={() => remove()}>Remove</button>
     </div>
   );
 };
--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,23 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { isClient } from './util';
 
 type Dispatch<A> = (value: A) => void;
 type SetStateAction<S> = S | ((prevState: S) => S);
 
-const useLocalStorage = <T>(key: string, initialValue?: T, raw?: boolean): [T, Dispatch<SetStateAction<T>>] => {
+const noop = () => {};
+
+const useLocalStorage = <T>(
+  key: string,
+  initialValue?: T,
+  raw?: boolean
+): [T | null, Dispatch<SetStateAction<T | null>>, () => void] => {
   if (!isClient) {
-    return [initialValue as T, () => {}];
+    return [initialValue as T, noop, noop];
   }
 
-  const [state, setState] = useState<T>(() => {
+  const [state, setState] = useState<T | null>(() => {
     try {
       const localStorageValue = localStorage.getItem(key);
+      if (typeof initialValue === 'undefined' && typeof localStorageValue !== 'string') {
+        return null;
+      }
       if (typeof localStorageValue !== 'string') {
         localStorage.setItem(key, raw ? String(initialValue) : JSON.stringify(initialValue));
         return initialValue;
-      } else {
-        return raw ? localStorageValue : JSON.parse(localStorageValue || 'null');
       }
+      return raw ? localStorageValue : JSON.parse(localStorageValue || 'null');
     } catch {
       // If user is in private mode or has storage restriction
       // localStorage can throw. JSON.parse and JSON.stringify
@@ -26,7 +34,18 @@ const useLocalStorage = <T>(key: string, initialValue?: T, raw?: boolean): [T, D
     }
   });
 
+  const remove = useCallback(() => {
+    try {
+      localStorage.removeItem(key);
+      setState(null);
+    } catch {
+      // If user is in private mode or has storage restriction
+      // localStorage can throw.
+    }
+  }, [key, setState]);
+
   useEffect(() => {
+    if (state === null) return;
     try {
       const serializedState = raw ? String(state) : JSON.stringify(state);
       localStorage.setItem(key, serializedState);
@@ -35,8 +54,7 @@ const useLocalStorage = <T>(key: string, initialValue?: T, raw?: boolean): [T, D
       // localStorage can throw. Also JSON.stringify can throw.
     }
   }, [state]);
-
-  return [state, setState];
+  return [state, setState, remove];
 };
 
 export default useLocalStorage;
