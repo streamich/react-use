2cf2ab00eb697b348d0ae4b52fcd424f2cb78bc7
0BE <othmane.bentaleb.95@gmail.com>
2019-12-14 16:19:38 +0100

    Review feedback: avoid swallowing errors

--- src/util.ts
+++ src/util.ts
@@ -1,23 +1,5 @@
 export const isClient = typeof window === 'object';
 
-export const on = (obj: any, ...args: any[]) => {
-  try {
-    obj.addEventListener(...args);
-  } catch (error) {
-    handleError(error);
-  }
-};
+export const on = (obj: any, ...args: any[]) => obj && obj.addEventListener(...args);
 
-export const off = (obj: any, ...args: any[]) => {
-  try {
-    obj.removeEventListener(...args);
-  } catch (error) {
-    handleError(error);
-  }
-};
-
-const handleError = (error: any) => {
-  if (process.env.NODE_ENV !== 'production') {
-    console.error(error);
-  }
-};
+export const off = (obj: any, ...args: any[]) => obj && obj.removeEventListener(...args);
