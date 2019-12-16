1620e019fff94fb4a7a711fd3121ec02c7e99301
suyingtao <suyingtao@baidu.com>
2019-12-16 12:59:03 +0800

    fix(useLocalStorage): using undefined for empty value instead of null

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -5,21 +5,22 @@ type Dispatch<A> = (value: A) => void;
 type SetStateAction<S> = S | ((prevState: S) => S);
 
 const noop = () => {};
+const isUndefined = (value?: any): boolean => typeof value === 'undefined';
 
 const useLocalStorage = <T>(
   key: string,
   initialValue?: T,
   raw?: boolean
-): [T | null, Dispatch<SetStateAction<T | null>>, () => void] => {
+): [T | undefined, Dispatch<SetStateAction<T | undefined>>, () => void] => {
   if (!isClient) {
     return [initialValue as T, noop, noop];
   }
 
-  const [state, setState] = useState<T | null>(() => {
+  const [state, setState] = useState<T | undefined>(() => {
     try {
       const localStorageValue = localStorage.getItem(key);
-      if (typeof initialValue === 'undefined' && typeof localStorageValue !== 'string') {
-        return null;
+      if (isUndefined(initialValue)) {
+        return undefined;
       }
       if (typeof localStorageValue !== 'string') {
         localStorage.setItem(key, raw ? String(initialValue) : JSON.stringify(initialValue));
@@ -37,7 +38,7 @@ const useLocalStorage = <T>(
   const remove = useCallback(() => {
     try {
       localStorage.removeItem(key);
-      setState(null);
+      setState(undefined);
     } catch {
       // If user is in private mode or has storage restriction
       // localStorage can throw.
@@ -45,7 +46,7 @@ const useLocalStorage = <T>(
   }, [key, setState]);
 
   useEffect(() => {
-    if (state === null) return;
+    if (isUndefined(state)) return;
     try {
       const serializedState = raw ? String(state) : JSON.stringify(state);
       localStorage.setItem(key, serializedState);
