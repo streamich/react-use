4ca2d9b29e0c2badb758ad14ec855f038b611de6
streamich <streamich@gmail.com>
2020-01-12 18:17:56 +0100

    test: 💍 add useScratch() stories

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,7 @@ export { default as useRaf } from './useRaf';
 export { default as useRafLoop } from './useRafLoop';
 export { default as useRafState } from './useRafState';
 export { default as useSearchParam } from './useSearchParam';
+export { default as useScratch } from './useScratch';
 export { default as useScroll } from './useScroll';
 export { default as useScrolling } from './useScrolling';
 export { default as useSessionStorage } from './useSessionStorage';
