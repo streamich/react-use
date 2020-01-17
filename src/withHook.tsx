989fadca3bc54bdef07c121240fe436e47a3aa40
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-17 20:58:17 +0530

    fix types

--- src/withHook.tsx
+++ src/withHook.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-type passedHook<T> = (props: React.Props<T>) => any;
+type passedHook<T> = (props: React.Props<T>) => object | undefined;
 
 function withHook<T>(useHook: passedHook<T>) {
-  return (WrappedComponent: React.ComponentType<T>) => (props: React.Props<T>) => {
+  return (WrappedComponent: React.ComponentType<T>) => (props: React.PropsWithChildren<T>) => {
     const propsToMapFromHook = useHook(props);
 
     return <WrappedComponent {...propsToMapFromHook} {...props} />;
