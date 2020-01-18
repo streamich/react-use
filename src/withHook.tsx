dffc814d315ef6708fa26eb22e94a68732d6ff64
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-18 09:19:01 +0530

    add usage of forwardRef

--- src/withHook.tsx
+++ src/withHook.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type passedHook<T> = (props: React.Props<T>) => object | undefined;
 
 function withHook<T>(useHook: passedHook<T>) {
-  return (WrappedComponent: React.ComponentType<T>) => (props: React.PropsWithChildren<T>) => {
-    const propsToMapFromHook = useHook(props);
+  return (WrappedComponent: React.ComponentType<T>) =>
+    forwardRef((props: React.PropsWithChildren<T>, ref) => {
+      const propsToMapFromHook = useHook(props);
 
-    return <WrappedComponent {...propsToMapFromHook} {...props} />;
-  };
+      return <WrappedComponent ref={ref} {...propsToMapFromHook} {...props} />;
+    });
 }
 
 export default withHook;
