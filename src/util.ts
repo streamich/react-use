4859d4ec1cacd6f93abf1e2b76b593b07a0990dd
streamich <streamich@gmail.com>
2020-02-15 12:06:35 +0100

    refactor: 💡 use React version for deep comparisons

--- src/util.ts
+++ src/util.ts
@@ -4,4 +4,4 @@ export const on = (obj: any, ...args: any[]) => obj.addEventListener(...args);
 
 export const off = (obj: any, ...args: any[]) => obj.removeEventListener(...args);
 
-export const isDeepEqual: (a: any, b: any) => boolean = require('fast-deep-equal/es6');
+export const isDeepEqual: (a: any, b: any) => boolean = require('fast-deep-equal/react');
