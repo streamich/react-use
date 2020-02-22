44a6cde00e1c74831d7c38b8ae4946f6f2171cf5
whincwu <whincwu@163.com>
2020-02-22 20:30:52 +0800

    feat: add useHash hook

---
+++ stories/useHash.story.tsx
@@ -0,0 +1,29 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { useHash, useMount } from '../src';
+import ShowDocs from './util/ShowDocs';
+
+const Demo = () => {
+  const [hash, setHash] = useHash();
+
+  useMount(() => {
+    setHash('#/path/to/page?userId=123');
+  });
+
+  return (
+    <div>
+      <div>window.location.href:</div>
+      <div>
+        <pre>{window.location.href}</pre>
+      </div>
+      <div>Edit hash: </div>
+      <div>
+        <input style={{ width: '100%' }} value={hash} onChange={e => setHash(e.target.value)} />
+      </div>
+    </div>
+  );
+};
+
+storiesOf('Sensors|useHash', module)
+  .add('Docs', () => <ShowDocs md={require('../docs/useHash.md')} />)
+  .add('Demo', () => <Demo />);
