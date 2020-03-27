4f6ebfea86ab57c1e22ef1c706bd868c49150db1
wmzy <1256573276@qq.com>
2020-03-27 14:10:56 +0800

    refactor(createGlobalState): remove store wrap

--- src/createGlobalState.ts
+++ src/createGlobalState.ts
@@ -2,26 +2,25 @@
 import { useLayoutEffect, useState } from 'react';
 
 export function createGlobalState<S = any>(initialState?: S) {
-  const store: { state: S | undefined; setState: (state: S) => void; setters: any[] } = {
-    state: initialState,
-    setState(state: S) {
-      store.state = state;
-      store.setters.forEach(setter => setter(store.state));
-    },
-    setters: [],
-  };
+  let state = initialState;
+  let setters: any[] = [];
+
+  function setState(newState: S) {
+    state = newState;
+    setters.forEach(setter => setter(state));
+  }
 
   return (): [S | undefined, (state: S) => void] => {
-    const [globalState, stateSetter] = useState<S | undefined>(store.state);
+    const [globalState, stateSetter] = useState<S | undefined>(state);
 
     useLayoutEffect(() => {
-      store.setters.push(stateSetter);
+      setters.push(stateSetter);
       return () => {
-        store.setters = store.setters.filter(setter => setter !== stateSetter);
+        setters = setters.filter(setter => setter !== stateSetter);
       }
     }, []);
 
-    return [globalState, store.setState];
+    return [globalState, setState];
   };
 }
 
