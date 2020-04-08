066a1e4f97fcdc11f58af61c27b61c77dd9db84b
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2020-04-08 08:43:53 +0200

    refactor: moved out reducer to improve memory usage

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -1,10 +1,9 @@
 import { useReducer, Reducer } from 'react';
 
+const toggleReducer = (state: boolean, nextValue?: any) => (typeof nextValue === 'boolean' ? nextValue : !state);
+
 const useToggle = (initialValue: boolean): [boolean, (nextValue?: any) => void] => {
-  return useReducer<Reducer<boolean, any>>(
-    (state: boolean, nextValue?: any) => (typeof nextValue === 'boolean' ? nextValue : !state),
-    initialValue
-  );
+  return useReducer<Reducer<boolean, any>>(toggleReducer, initialValue);
 };
 
 export default useToggle;
