af57ffd158b5fd7d50a7808fb002cf86166a4944
LeoooY <leoy.dev@gmail.com>
2020-06-06 16:41:04 +0800

    feat: improve implementation of useFirstMountState() hook

--- src/useFirstMountState.ts
+++ src/useFirstMountState.ts
@@ -1,13 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 export function useFirstMountState(): boolean {
   const isFirst = useRef(true);
 
-  if (isFirst.current) {
-    isFirst.current = false;
-
-    return true;
-  }
+  useEffect(() => {
+    isFirst.current = false
+  }, [])
 
   return isFirst.current;
 }
