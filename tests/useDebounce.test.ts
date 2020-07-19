1e9f4d519ad66b9336e84836714ba3fb7f7d6ac3
Sebastian Holstein <mail@sebastian-holstein.de>
2020-07-19 22:47:27 +0200

    chore(useDebounce): fix test description

--- tests/useDebounce.test.ts
+++ tests/useDebounce.test.ts
@@ -20,7 +20,7 @@ describe('useDebounce', () => {
     expect(useDebounce).toBeDefined();
   });
 
-  it('should return three functions', () => {
+  it('should return two functions', () => {
     const hook = renderHook(() => useDebounce(() => {}, 5));
 
     expect(hook.result.current.length).toBe(2);
