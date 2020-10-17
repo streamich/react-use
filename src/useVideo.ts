ac4dd786f42a2c59bdaaf9ddebe2e830982d7fcc
armin <arminyahyaa@gmail.com>
2020-10-17 14:50:53 +0330

    fix: add generic typing to createHTMLMediaHook. no typecheck problem with ref anymore.

--- src/useVideo.ts
+++ src/useVideo.ts
@@ -1,5 +1,5 @@
 import createHTMLMediaHook from './factory/createHTMLMediaHook';
 
-const useVideo = createHTMLMediaHook('video');
+const useVideo = createHTMLMediaHook<HTMLVideoElement>('video');
 
 export default useVideo;
