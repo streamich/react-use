577d4363e3e44d9f48e7f11aa7d6411ee4784e24
Aleksei Gurianov <gurianov@gmail.com>
2020-11-09 18:30:24 +0400

    test(createGlobalState): cover typings overload

--- tests/createGlobalState.test.ts
+++ tests/createGlobalState.test.ts
@@ -18,4 +18,15 @@ describe('useGlobalState', () => {
     expect(result1.current[0] === 1);
     expect(result2.current[0] === 1);
   });
+
+  it('do not have undefined on current value with initialState provided', () => {
+    const useTest1 = createGlobalState<string>();
+    const [test1] = useTest1();
+    // @ts-expect-error
+    test1.charAt(0);
+
+    const useTest2 = createGlobalState<string>('asd');
+    const [test2] = useTest2();
+    test2.charAt(0);
+  });
 });
