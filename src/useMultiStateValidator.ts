acff81d99abdbbefcc2985297ee01c3cda9ef4c9
xobotyi <xog3@yandex.ru>
2021-01-08 22:26:27 +0300

    fix: improve useStateValidator and useMultiStateValidator typings.
    Now it is compatible with TS 4.x.x

--- src/useMultiStateValidator.ts
+++ src/useMultiStateValidator.ts
@@ -4,10 +4,10 @@ import { StateValidator, UseStateValidatorReturn, ValidityState } from './useSta
 export type MultiStateValidatorStates = any[] | { [p: string]: any } | { [p: number]: any };
 export type MultiStateValidator<V extends ValidityState, S extends MultiStateValidatorStates> = StateValidator<V, S>;
 
-export function useMultiStateValidator<V extends ValidityState, S extends MultiStateValidatorStates, I extends V>(
+export function useMultiStateValidator<V extends ValidityState, S extends MultiStateValidatorStates>(
   states: S,
   validator: MultiStateValidator<V, S>,
-  initialValidity: I = [undefined] as I
+  initialValidity: V = [undefined] as V
 ): UseStateValidatorReturn<V> {
   if (typeof states !== 'object') {
     throw new Error('states expected to be an object or array, got ' + typeof states);
