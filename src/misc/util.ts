f27a05fe524dd4a921146277ad0e08b38fc0e5d0
xobotyi <xog3@yandex.ru>
2021-01-31 03:03:51 +0300

    feat: improve `on` and `off` util functions typing.

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -1,7 +1,21 @@
 export const noop = () => {};
 
-export const on = (obj: any, ...args: any[]) => obj.addEventListener(...args);
+export function on<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T['addEventListener']> | [string, Function | null, ...any]
+): void {
+  if (obj && obj.addEventListener) {
+    obj.addEventListener(...(args as Parameters<HTMLElement['addEventListener']>));
+  }
+}
 
-export const off = (obj: any, ...args: any[]) => obj.removeEventListener(...args);
+export function off<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T['removeEventListener']> | [string, Function | null, ...any]
+): void {
+  if (obj && obj.removeEventListener) {
+    obj.removeEventListener(...(args as Parameters<HTMLElement['removeEventListener']>));
+  }
+}
 
 export const isBrowser = typeof window === 'object';
