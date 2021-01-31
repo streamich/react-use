14362cc1a748ec03928a5fdf30bec8ef5b03025b
xobotyi <xog3@yandex.ru>
2021-01-31 11:28:36 +0300

    chore(useUpdate): improve `useUpdate` test suite

--- tests/useUpdate.test.ts
+++ tests/useUpdate.test.ts
@@ -1,35 +1,70 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import useUpdate from '../src/useUpdate';
 
-it('should init update function', () => {
-  const { result } = renderHook(() => useUpdate());
-  const update = result.current;
-
-  expect(update).toBeInstanceOf(Function);
-});
-
-it('should forces a re-render every time update function is called', () => {
-  let renderCount = 0;
-  const { result } = renderHook(() => {
-    renderCount++;
-    return useUpdate();
+describe('useUpdate', () => {
+  it('should be defined', () => {
+    expect(useUpdate).toBeDefined();
   });
-  const update = result.current;
 
-  expect(renderCount).toBe(1);
+  it('should return a function', () => {
+    const { result } = renderHook(() => useUpdate());
 
-  act(() => update());
-  expect(renderCount).toBe(2);
+    expect(typeof result.current).toBe('function');
+  })
 
-  act(() => update());
-  expect(renderCount).toBe(3);
-});
+  it('should re-render component each time returned function is called', () => {
+    let renders = 0;
+    const { result: { current: update } } = renderHook(() => {
+      renders++;
+      return useUpdate();
+    });
 
-it('should return same update function instance on each update', () => {
-  const { result, rerender } = renderHook(() => useUpdate());
-  const { current: updateCb } = result;
+    expect(renders).toBe(1);
 
-  rerender();
+    act(() => update());
+    expect(renders).toBe(2);
 
-  expect(Object.is(result.current, updateCb)).toBe(true);
+    act(() => update());
+    expect(renders).toBe(3);
+  })
+
+  it('should return exact same function in between renders', () => {
+    let renders = 0;
+    const { result } = renderHook(() => {
+      renders++;
+      return useUpdate();
+    });
+    let initialUpdateFn = result.current;
+
+    expect(renders).toBe(1);
+
+    act(() => result.current());
+    expect(renders).toBe(2);
+    expect(initialUpdateFn).toBe(result.current);
+
+    act(() => result.current());
+    expect(renders).toBe(3);
+    expect(initialUpdateFn).toBe(result.current);
+  })
+
+  it('passing the argument to returned function should not affect the use', () => {
+    let renders = 0;
+    const { result } = renderHook(() => {
+      renders++;
+      return useUpdate();
+    });
+    let initialUpdateFn = result.current;
+
+    expect(renders).toBe(1);
+
+    /* @ts-expect-error */
+    act(() => result.current(1));
+    expect(renders).toBe(2);
+    expect(initialUpdateFn).toBe(result.current);
+
+    /* @ts-expect-error */
+    act(() => result.current(1));
+    expect(renders).toBe(3);
+    expect(initialUpdateFn).toBe(result.current);
+  })
 });
