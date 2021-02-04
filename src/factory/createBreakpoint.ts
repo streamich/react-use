14c9f411b6373b4718457314badb7970aa29d7f1
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2021-02-04 12:18:33 +0100

    Default to window.innerWidth instead of 0 in createBreakpoint

--- src/factory/createBreakpoint.ts
+++ src/factory/createBreakpoint.ts
@@ -1,10 +1,10 @@
 import { useEffect, useMemo, useState } from 'react';
-import { off, on } from '../misc/util';
+import { isBrowser, off, on } from '../misc/util';
 
 const createBreakpoint = (
   breakpoints: { [name: string]: number } = { laptopL: 1440, laptop: 1024, tablet: 768 }
 ) => () => {
-  const [screen, setScreen] = useState(0);
+  const [screen, setScreen] = useState(isBrowser ? window.innerWidth : 0);
 
   useEffect(() => {
     const setSideScreen = (): void => {
