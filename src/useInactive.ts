5667fef6768e4581222231040dabb7d655b21925
tkwant <t.kwant1@gmail.com>
2021-02-14 22:12:00 +0100

    remove old comments

--- src/useInactive.ts
+++ src/useInactive.ts
@@ -1,18 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import { off, on } from './misc/util';
-
-// export default () => {
-//   const [mouseWheelScrolled, setMouseWheelScrolled] = useState(0);
-//   useEffect(() => {
-//     const updateScroll = (e: MouseWheelEvent) => {
-//       setMouseWheelScrolled(e.deltaY + mouseWheelScrolled);
-//     };
-//     on(window, 'wheel', updateScroll, false);
-//     return () => off(window, 'wheel', updateScroll);
-//   });
-//   return mouseWheelScrolled;
-// };
-
 import { useEffect, useState } from 'react';
 export default (startTime) => {
   const [timer, setTimer] = useState(startTime);
