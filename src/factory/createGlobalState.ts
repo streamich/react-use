0713ed68c6b35f8e8a0b8194d78b5482ec7fa6a4
Juan Lulkin <jmaiz@spotify.com>
2021-02-15 20:51:55 +0100

    feat:Makes createGlobalState API compatible with React

--- src/factory/createGlobalState.ts
+++ src/factory/createGlobalState.ts
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useState, SetStateAction } from 'react';
 import useEffectOnce from '../useEffectOnce';
 import useIsomorphicLayoutEffect from '../useIsomorphicLayoutEffect';
 
-export function createGlobalState<S = any>(initialState?: S) {
-  const store: { state: S | undefined; setState: (state: S) => void; setters: any[] } = {
-    state: initialState,
-    setState(state: S) {
-      store.state = state;
+export function createGlobalState<S = any>(
+  initialState: S | (() => S)
+): () => [S, (state: SetStateAction<S>) => void];
+export function createGlobalState<S = undefined>(): () => [S, (state: SetStateAction<S>) => void];
+
+export function createGlobalState<S>(initialState?: S) {
+  const store: {
+    state: S;
+    setState: (state: SetStateAction<S>) => void;
+    setters: any[];
+  } = {
+    state: initialState instanceof Function ? initialState() : initialState,
+    setState(stateOrFn: SetStateAction<S>) {
+      store.state =
+        stateOrFn instanceof Function ? stateOrFn(this.state || (initialState as S)) : stateOrFn;
       store.setters.forEach((setter) => setter(store.state));
     },
     setters: [],
   };
 
-  return (): [S | undefined, (state: S) => void] => {
+  return () => {
     const [globalState, stateSetter] = useState<S | undefined>(store.state);
 
     useEffectOnce(() => () => {
--- tests/createGlobalState.test.ts
+++ tests/createGlobalState.test.ts
@@ -1,21 +1,73 @@
-import { act, renderHook } from '@testing-library/react-hooks';
-import createGlobalState from '../src/factory/createGlobalState';
+import { act, renderHook } from "@testing-library/react-hooks";
+import createGlobalState from "../src/factory/createGlobalState";
 
-describe('useGlobalState', () => {
-  it('should be defined', () => {
+describe("useGlobalState", () => {
+  it("should be defined", () => {
     expect(createGlobalState).toBeDefined();
   });
 
-  it('both components should be updated', () => {
+  it("both components should be updated", () => {
     const useGlobalValue = createGlobalState(0);
     const { result: result1 } = renderHook(() => useGlobalValue());
     const { result: result2 } = renderHook(() => useGlobalValue());
-    expect(result1.current[0] === 0);
-    expect(result2.current[0] === 0);
+    expect(result1.current[0]).toBe(0);
+    expect(result2.current[0]).toBe(0);
     act(() => {
       result1.current[1](1);
     });
-    expect(result1.current[0] === 1);
-    expect(result2.current[0] === 1);
+    expect(result1.current[0]).toBe(1);
+    expect(result2.current[0]).toBe(1);
+  });
+
+  it("allows setting state with function", () => {
+    const useGlobalValue = createGlobalState(0);
+    const { result: result1 } = renderHook(() => useGlobalValue());
+    const { result: result2 } = renderHook(() => useGlobalValue());
+    expect(result1.current[0]).toBe(0);
+    expect(result2.current[0]).toBe(0);
+    act(() => {
+      result1.current[1]((value) => value + 1);
+    });
+    expect(result1.current[0]).toBe(1);
+    expect(result2.current[0]).toBe(1);
+  });
+
+  it("initializes with undefined", () => {
+    const useGlobalValue = createGlobalState<number>();
+    const { result: result1 } = renderHook(() => useGlobalValue());
+    const { result: result2 } = renderHook(() => useGlobalValue());
+    expect(result1.current[0]).toBe(undefined);
+    expect(result2.current[0]).toBe(undefined);
+    act(() => {
+      // this is the only case where types fail, it should guard the number
+      result1.current[1]((value) => value);
+    });
+    expect(result1.current[0]).toBe(undefined);
+    expect(result2.current[0]).toBe(undefined);
+  });
+
+  it("initializes with undefined", () => {
+    const useGlobalValue = createGlobalState();
+    const { result: result1 } = renderHook(() => useGlobalValue());
+    const { result: result2 } = renderHook(() => useGlobalValue());
+    expect(result1.current[0]).toBe(undefined);
+    expect(result2.current[0]).toBe(undefined);
+    act(() => {
+      // @ts-expect-error this case it checks correctly, hence the comment
+      result1.current[1]((value) => 1);
+    });
+  });
+
+  it("initializes with function", () => {
+    const useGlobalValue = createGlobalState(() => 0);
+    const { result: result1 } = renderHook(() => useGlobalValue());
+    const { result: result2 } = renderHook(() => useGlobalValue());
+    expect(result1.current[0]).toBe(0);
+    expect(result2.current[0]).toBe(0);
+    act(() => {
+      result1.current[1](1);
+    });
+    expect(result1.current[0]).toBe(1);
+    expect(result2.current[0]).toBe(1);
   });
 });
