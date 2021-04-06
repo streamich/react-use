51bf4e915431454c97fb224be51e1a1bff11c6bf
xobotyi <xog3@yandex.ru>
2021-04-06 11:38:36 +0300

    fix: lint issues.

--- src/useSetState.ts
+++ src/useSetState.ts
@@ -4,14 +4,11 @@ const useSetState = <T extends object>(
   initialState: T = {} as T
 ): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
   const [state, set] = useState<T>(initialState);
-  const setState = useCallback(
-    (patch) => {
-      set((prevState) =>
-        Object.assign({}, prevState, patch instanceof Function ? patch(prevState) : patch)
-      );
-    },
-    []
-  );
+  const setState = useCallback((patch) => {
+    set((prevState) =>
+      Object.assign({}, prevState, patch instanceof Function ? patch(prevState) : patch)
+    );
+  }, []);
 
   return [state, setState];
 };
