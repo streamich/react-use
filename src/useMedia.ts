be2b0e523806f07df5d39ba6311210d03f1de570
teagueStockwell <tsAppDevelopment@gmail.com>
2021-11-08 01:57:27 -0800

    fix: useMedia SSR hydration bug with defaultState
    Prevent a React hydration mismatch when a default value is provided
    by not defaulting to window.matchMedia(query).matches.

--- src/useMedia.ts
+++ src/useMedia.ts
@@ -1,10 +1,27 @@
 import { useEffect, useState } from 'react';
 import { isBrowser } from './misc/util';
 
-const useMedia = (query: string, defaultState: boolean = false) => {
-  const [state, setState] = useState(
-    isBrowser ? () => window.matchMedia(query).matches : defaultState
-  );
+const getInitialState = (query: string, defaultState?: boolean) => {
+  // Prevent a React hydration mismatch when a default value is provided by not defaulting to window.matchMedia(query).matches.
+  if (defaultState !== undefined) {
+    return defaultState;
+  }
+
+  // If a default value is not provided, and you are rendering on the server, warn of a possible hydration mismatch when defaulting to false.
+  if (!isBrowser) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.warn(
+        '`useMedia` When server side rendering, defaultState should be defined to prevent a hydration mismatches.'
+      );
+    }
+    return false;
+  }
+
+  return window.matchMedia(query).matches;
+};
+
+const useMedia = (query: string, defaultState?: boolean) => {
+  const [state, setState] = useState(getInitialState(query, defaultState));
 
   useEffect(() => {
     let mounted = true;
