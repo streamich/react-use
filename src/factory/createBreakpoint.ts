2000c96158b70264667194937273ce1e6456bc76
Armando Magalhães <armando.mag95@gmail.com>
2021-11-19 11:09:01 +0000

    fix(createBreakpoint): use function overloading types instead

--- src/factory/createBreakpoint.ts
+++ src/factory/createBreakpoint.ts
@@ -6,36 +6,38 @@ export const defaultBreakpoints = { laptopL: 1440, laptop: 1024, tablet: 768 };
 export type DefaultBreakpoints = typeof defaultBreakpoints;
 export type DefaultBreakpointKeys = keyof DefaultBreakpoints;
 
-export type CreateBreakpointFunction = {
-  (breakpoints: undefined): () => DefaultBreakpointKeys;
-  <T extends BreakpointSelectors>(breakpoints: T): () => keyof T;
-}
+function createBreakpoint(): () => DefaultBreakpointKeys;
+function createBreakpoint<T extends BreakpointSelectors>(breakpoints: T): () => keyof T;
+function createBreakpoint(breakpoints = defaultBreakpoints) {
+  const useBreakpoint = () => {
+    const [screen, setScreen] = useState(isBrowser ? window.innerWidth : 0);
 
-const createBreakpoint: CreateBreakpointFunction = (breakpoints = defaultBreakpoints) => () => {
-  const [screen, setScreen] = useState(isBrowser ? window.innerWidth : 0);
- 
-  useEffect(() => {
-    const setSideScreen = (): void => {
-      setScreen(window.innerWidth);
-    };
-    setSideScreen();
-    on(window, 'resize', setSideScreen);
-    return () => {
-      off(window, 'resize', setSideScreen);
-    };
-  });
-  const sortedBreakpoints = useMemo(
-    () => Object.entries(breakpoints).sort((a, b) => (a[1] >= b[1] ? 1 : -1)),
-    [breakpoints]
-  );
-  const result = sortedBreakpoints.reduce((acc, [name, width]) => {
-    if (screen >= width) {
-      return name;
-    } else {
-      return acc;
-    }
-  }, sortedBreakpoints[0][0]);
-  return result;
-};
+    useEffect(() => {
+      const setSideScreen = (): void => {
+        setScreen(window.innerWidth);
+      };
+      setSideScreen();
+      on(window, 'resize', setSideScreen);
+      return () => {
+        off(window, 'resize', setSideScreen);
+      };
+    });
+    const sortedBreakpoints = useMemo(
+      () => Object.entries(breakpoints).sort((a, b) => (a[1] >= b[1] ? 1 : -1)),
+      [breakpoints]
+    );
+    const result = sortedBreakpoints.reduce((acc, [name, width]) => {
+      if (screen >= width) {
+        return name;
+      } else {
+        return acc;
+      }
+    }, sortedBreakpoints[0][0]);
+
+    return result;
+  };
+
+  return useBreakpoint;
+}
 
 export default createBreakpoint;
