dc5567ae1aee29d2e0d0205c79cfaf2a8589efa4
Armando Magalhães <armando.mag95@gmail.com>
2021-11-19 11:52:37 +0100

    fix(createBreakpoint): improve the return type
    before this commit, when using the createBreakpoint() function with or without a custom breakpoint, it will always return a type `() => string`.
    
    this commit adds overloaded and mapped types to the createBreakpoint function definition that reduces the cardinality of the type results according to the given parameters.
    
    in case no custom breakpoints are given, the keys from the default object are resolved as the return type of the useBreakpoint function

--- src/factory/createBreakpoint.ts
+++ src/factory/createBreakpoint.ts
@@ -1,33 +1,41 @@
 import { useEffect, useMemo, useState } from 'react';
 import { isBrowser, off, on } from '../misc/util';
 
-const createBreakpoint =
-  (breakpoints: { [name: string]: number } = { laptopL: 1440, laptop: 1024, tablet: 768 }) =>
-  () => {
-    const [screen, setScreen] = useState(isBrowser ? window.innerWidth : 0);
+export type BreakpointSelectors = { [name: string]: number };
+export const defaultBreakpoints = { laptopL: 1440, laptop: 1024, tablet: 768 };
+export type DefaultBreakpoints = typeof defaultBreakpoints;
+export type DefaultBreakpointKeys = keyof DefaultBreakpoints;
 
-    useEffect(() => {
-      const setSideScreen = (): void => {
-        setScreen(window.innerWidth);
-      };
-      setSideScreen();
-      on(window, 'resize', setSideScreen);
-      return () => {
-        off(window, 'resize', setSideScreen);
-      };
-    });
-    const sortedBreakpoints = useMemo(
-      () => Object.entries(breakpoints).sort((a, b) => (a[1] >= b[1] ? 1 : -1)),
-      [breakpoints]
-    );
-    const result = sortedBreakpoints.reduce((acc, [name, width]) => {
-      if (screen >= width) {
-        return name;
-      } else {
-        return acc;
-      }
-    }, sortedBreakpoints[0][0]);
-    return result;
-  };
+export type CreateBreakpointFunction = {
+  (breakpoints: undefined): () => DefaultBreakpointKeys;
+  <T extends BreakpointSelectors>(breakpoints: T): () => keyof T;
+}
+
+const createBreakpoint: CreateBreakpointFunction = (breakpoints = defaultBreakpoints) => () => {
+  const [screen, setScreen] = useState(isBrowser ? window.innerWidth : 0);
+ 
+  useEffect(() => {
+    const setSideScreen = (): void => {
+      setScreen(window.innerWidth);
+    };
+    setSideScreen();
+    on(window, 'resize', setSideScreen);
+    return () => {
+      off(window, 'resize', setSideScreen);
+    };
+  });
+  const sortedBreakpoints = useMemo(
+    () => Object.entries(breakpoints).sort((a, b) => (a[1] >= b[1] ? 1 : -1)),
+    [breakpoints]
+  );
+  const result = sortedBreakpoints.reduce((acc, [name, width]) => {
+    if (screen >= width) {
+      return name;
+    } else {
+      return acc;
+    }
+  }, sortedBreakpoints[0][0]);
+  return result;
+};
 
 export default createBreakpoint;
