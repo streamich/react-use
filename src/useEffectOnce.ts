5bc452493a75332d8c990171e499c7b0e93d9142
Manuel Deschamps <manuel@digits.com>
2023-02-08 17:36:05 -0500

    Update useEffectOnce to run once in strictmode
    In React18 StrictMode, effects without dependents can run multiple times. This ref prevents effect callback to run more than once.

--- src/useEffectOnce.ts
+++ src/useEffectOnce.ts
@@ -1,7 +1,12 @@
-import { EffectCallback, useEffect } from 'react';
+import { EffectCallback, useEffect, useRef } from "react"
 
 const useEffectOnce = (effect: EffectCallback) => {
-  useEffect(effect, []);
-};
+  const runOnce = useRef(false)
+  useEffect(() => {
+    if (runOnce.current) return
+    runOnce.current = true
+    return effect()
+  }, [])
+}
 
-export default useEffectOnce;
+export default useEffectOnce
