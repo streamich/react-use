81accc5a54db4c0e8303374cb6a969e9c5c0cc68
molvqingtai <molvqingtai@gmail.com>
2023-11-10 14:13:47 +0800

    fix(useClickAway): Fixed an issue where clicking on yourself in shadowDOM also triggered a callback

--- src/useClickAway.ts
+++ src/useClickAway.ts
@@ -1,7 +1,7 @@
-import { RefObject, useEffect, useRef } from 'react';
-import { off, on } from './misc/util';
+import { RefObject, useEffect, useRef } from "react";
+import { off, on } from "./misc/util";
 
-const defaultEvents = ['mousedown', 'touchstart'];
+const defaultEvents = ["mousedown", "touchstart"];
 
 const useClickAway = <E extends Event = Event>(
   ref: RefObject<HTMLElement | null>,
@@ -13,16 +13,33 @@ const useClickAway = <E extends Event = Event>(
     savedCallback.current = onClickAway;
   }, [onClickAway]);
   useEffect(() => {
+    const { current: el } = ref;
+    if (!el) return;
+
+    const rootNode = el.getRootNode();
+    const isInShadow = rootNode instanceof ShadowRoot;
+
+    /**
+     * When events are captured outside the component, events that occur in shadow DOM will target the host element
+     * so additional event listeners need to be added for shadowDom
+     *
+     *  document       shadowDom          target
+     *    |                |                 |
+     *    |- on(document) -|-  on(rootNode) -|
+     */
     const handler = (event) => {
-      const { current: el } = ref;
-      el && !el.contains(event.target) && savedCallback.current(event);
+      !el.contains(event.target) &&
+        event.target.shadowRoot !== rootNode &&
+        savedCallback.current(event);
     };
     for (const eventName of events) {
       on(document, eventName, handler);
+      isInShadow && on(rootNode, eventName, handler);
     }
     return () => {
       for (const eventName of events) {
         off(document, eventName, handler);
+        isInShadow && off(rootNode, eventName, handler);
       }
     };
   }, [events, ref]);
