bd81af469708b22ac23b23d9db15c6525eb0188a
Gavin <gaving1226@gmail.com>
2024-06-14 15:45:40 -0400

    feat: adding restoreOriginalOnUnmount, test code written but failing

--- src/useTitle.ts
+++ src/useTitle.ts
@@ -2,26 +2,37 @@ import { useEffect, useRef } from 'react';
 
 export interface UseTitleOptions {
   restoreOnUnmount?: boolean;
+  restoreOriginalOnUnmount?: boolean;
 }
 
 const DEFAULT_USE_TITLE_OPTIONS: UseTitleOptions = {
   restoreOnUnmount: false,
+  restoreOriginalOnUnmount: false,
 };
 
 function useTitle(title: string, options: UseTitleOptions = DEFAULT_USE_TITLE_OPTIONS) {
   const prevTitleRef = useRef(document.title);
-
+  const originalRender = useRef(true);
+  
   if (document.title !== title) document.title = title;
 
   useEffect(() => {
-    if (options && options.restoreOnUnmount) {
-      return () => {
-        document.title = prevTitleRef.current;
-      };
-    } else {
-      return;
+    if (options) {
+      if (options.restoreOnUnmount) {
+        return () => {
+          document.title = prevTitleRef.current;
+        };
+      } else if (options.restoreOriginalOnUnmount) {
+        return () => {
+          document.title = originalTitle;
+        }
+      } else {
+        return;
+      }
     }
-  }, []);
+
+    return;
+  }, [title]);
 }
 
 export default typeof document !== 'undefined' ? useTitle : (_title: string) => {};
